9070edd78a08dc1fb136d97dc058fa25ad9e07aa
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-18 18:48:15 +0200

    CREATE: Función ft_strncmp (código de la piscina, ligeramente cambiado)

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -12,8 +12,20 @@
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	char	*str1;
+	char	*str2;
 
+	str1 = (char *)s1;
+	str2 = (char *)s2;
+	while (*str1 != '\0' && *str2 != '\0' && *str1 == *str2 && n > 0)
+	{
+		n--;
+		str1++;
+		str2++;
+	}
+	if (n == 0)
+		return (0);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
-
--- libft/libft.h
+++ libft/libft.h
@@ -27,5 +27,4 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
-
 #endif /* libft.h */
