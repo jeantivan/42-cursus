0c68da66e66010066762f0d506f4c3054eca904c
Jean <jptivan53@gmail.com>
2024-09-23 22:53:36 +0200

    CHANGE: Estaba retornando la resta de punteros incorrecta

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -27,5 +27,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	if (n == 0)
 		return (0);
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+	return ((unsigned char)*str1 - (unsigned char)*str2);
 }
