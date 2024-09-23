9b21afe560f72a0dfaf407c15ec276cefbb28711
Jean <jptivan53@gmail.com>
2024-09-23 21:41:37 +0200

    ADD: Función pasa los test de libft-war-machine

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -12,48 +12,21 @@
 
 #include "libft.h"
 
-/* size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t		i;
-	size_t		size_to_cpy;
-	size_t		src_len;
+	size_t	src_len;
+	size_t	i;
 
-	size_to_cpy = size;
 	src_len = ft_strlen(src);
 	i = 0;
-	if (size_to_cpy != 0)
+	if (size > 0)
 	{
-		while (i < size_to_cpy && src[i] == '\0')
+		while (i < size - 1 && src[i] != '\0')
 		{
 			dst[i] = src[i];
 			i++;
 		}
+		dst[i] = '\0';
 	}
-	dst[i] = '\0';
 	return (src_len);
-} */
-size_t	ft_strlcpy(char *dst, const char *src, size_t dsize)
-{
-	const char	*osrc;
-	size_t		nleft;
-
-	osrc = src;
-	nleft = dsize;
-	if (nleft != 0)
-	{
-		while (--nleft != 0)
-		{
-			*dst++ = *src++;
-			if (*dst == '\0')
-				break ;
-		}
-	}
-	if (nleft == 0)
-	{
-		if (dsize != 0)
-			*dst = '\0';
-		while (*src++)
-			;
-	}
-	return (src - osrc - 1);
 }
