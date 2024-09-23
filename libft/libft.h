03d8d1b3d2377e674f27259c615eab114b8497fe
Jean <jptivan53@gmail.com>
2024-09-23 22:54:30 +0200

    ADD: Prototipo de la función ft_memcmp

--- libft/libft.h
+++ libft/libft.h
@@ -29,6 +29,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
