7c9a646b1905f7f0d1dbc1f8554cf79b4763565c
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-24 19:39:28 +0200

    Probando la lógica para el ft_itoa

--- libft/main.c
+++ libft/main.c
@@ -26,68 +26,9 @@ void	ft_print_result(char const *s)
 	write(1, s, len);
 }
 
-int		main(int argc, const char *argv[])
+int		main()
 {
-	char	str[] = "lorem ipsum dolor sit amet";
-	char	*strsub;
-	int		arg;
-
-	alarm(5);
-	if (argc == 1)
-		return (0);
-	else if ((arg = atoi(argv[1])) == 1)
-	{
-		if (!(strsub = ft_substr(str, 0, 10)))
-			ft_print_result("NULL");
-		else
-			ft_print_result(strsub);
-		if (str == strsub)
-			ft_print_result("\nA new string was not returned");
-	}
-	else if (arg == 2)
-	{
-		if (!(strsub = ft_substr(str, 7, 10)))
-			ft_print_result("NULL");
-		else
-			ft_print_result(strsub);
-		if (str == strsub)
-			ft_print_result("\nA new string was not returned");
-	}
-	else if (arg == 3)
-	{
-		if (!(strsub = ft_substr(str, 7, 0)))
-			ft_print_result("NULL");
-		else
-			ft_print_result(strsub);
-		if (str == strsub)
-			ft_print_result("\nA new string was not returned");
-	}
-	else if (arg == 4)
-	{
-		if (!(strsub = ft_substr(str, 0, 0)))
-			ft_print_result("NULL");
-		else
-			ft_print_result(strsub);
-		if (str == strsub)
-			ft_print_result("\nA new string was not returned");
-	}
-	else if (arg == 5)
-	{
-		char *bullshit;
-		if (!(strsub = ft_substr(str, 400, 20)))
-			ft_print_result("NULL");
-		else
-		{
-			bullshit = (char *)&strsub[30];
-			bullshit = "FULL BULLSHIT";
-			if (strsub)
-				ft_print_result(strsub);
-			else
-				ft_print_result("rip");
-		}
-		if (str == strsub)
-			ft_print_result("\nA new string was not returned");
-		(void)bullshit;
-	}
+	printf("1 / 10 = %i\n", 1 / 10);
+	ft_itoa(-202100);
 	return (0);
 }
