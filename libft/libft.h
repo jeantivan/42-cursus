2fb5c7bb7f5f76e45cf27f42ad9988fc79b6746f
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-24 18:43:47 +0200

    ADD: Prototipo de ft_strjoin

--- libft/libft.h
+++ libft/libft.h
@@ -38,7 +38,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
-char	*ft_substr(char const *s, unsigned int start,
-size_t len);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif /* libft.h */
