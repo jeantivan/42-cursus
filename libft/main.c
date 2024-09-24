963a8a34ddb3d189082e2cc5b6d5102b135eaa0e
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-24 18:16:34 +0200

    ADD: Test para ft_substr

--- libft/main.c
+++ libft/main.c
@@ -28,81 +28,66 @@ void	ft_print_result(char const *s)
 
 int		main(int argc, const char *argv[])
 {
-	const char *str;
-	int			arg;
+	char	str[] = "lorem ipsum dolor sit amet";
+	char	*strsub;
+	int		arg;
 
 	alarm(5);
 	if (argc == 1)
 		return (0);
 	else if ((arg = atoi(argv[1])) == 1)
 	{
-		if (!(str = ft_strnstr("lorem ipsum dolor sit amet", "lorem", 15)))
+		if (!(strsub = ft_substr(str, 0, 10)))
 			ft_print_result("NULL");
 		else
-			ft_print_result(str);
+			ft_print_result(strsub);
+		if (str == strsub)
+			ft_print_result("\nA new string was not returned");
 	}
 	else if (arg == 2)
 	{
-		if (!(str = ft_strnstr("lorem ipsum dolor sit amet", "ipsum", 15)))
+		if (!(strsub = ft_substr(str, 7, 10)))
 			ft_print_result("NULL");
 		else
-			ft_print_result(str);
+			ft_print_result(strsub);
+		if (str == strsub)
+			ft_print_result("\nA new string was not returned");
 	}
 	else if (arg == 3)
 	{
-		if (!(str = ft_strnstr("lorem ipsum dolor sit lorem ipsum dolor", "ipsum", 35)))
+		if (!(strsub = ft_substr(str, 7, 0)))
 			ft_print_result("NULL");
 		else
-			ft_print_result(str);
+			ft_print_result(strsub);
+		if (str == strsub)
+			ft_print_result("\nA new string was not returned");
 	}
 	else if (arg == 4)
 	{
-		if (!(str = ft_strnstr("lorem ipsum dolor sit amet", "", 10)))
+		if (!(strsub = ft_substr(str, 0, 0)))
 			ft_print_result("NULL");
 		else
-			ft_print_result(str);
+			ft_print_result(strsub);
+		if (str == strsub)
+			ft_print_result("\nA new string was not returned");
 	}
 	else if (arg == 5)
 	{
-		if (!(str = ft_strnstr("lorem ipsum dolor sit amet", "ipsumm", 30)))
+		char *bullshit;
+		if (!(strsub = ft_substr(str, 400, 20)))
 			ft_print_result("NULL");
 		else
-			ft_print_result(str);
-	}
-	else if (arg == 6)
-	{
-		if (!(str = ft_strnstr("lorem ipsum dolor sit amet", "dol", 30)))
-			ft_print_result("NULL");
-		else
-			ft_print_result(str);
-	}
-	else if (arg == 7)
-	{
-		if (!(str = ft_strnstr("lorem ipsum dolor sit amet", "consectetur", 30)))
-			ft_print_result("NULL");
-		else
-			ft_print_result(str);
-	}
-	else if (arg == 8)
-	{
-		if (!(str = ft_strnstr("lorem ipsum dolor sit amet", "sit", 10)))
-			ft_print_result("NULL");
-		else
-			ft_print_result(str);
-	}
-	else if (arg == 9)
-	{
-		if (!(str = ft_strnstr("lorem ipsum dolor sit amet", "dolor", 15)))
-			ft_print_result("NULL");
-		else
-			ft_print_result(str);
-	}
-	else if (arg == 10)
-	{
-		if (!(str = ft_strnstr("lorem ipsum dolor sit amet", "dolor", 0)))
-			ft_print_result("NULL");
-		else
-			ft_print_result(str);
+		{
+			bullshit = (char *)&strsub[30];
+			bullshit = "FULL BULLSHIT";
+			if (strsub)
+				ft_print_result(strsub);
+			else
+				ft_print_result("rip");
+		}
+		if (str == strsub)
+			ft_print_result("\nA new string was not returned");
+		(void)bullshit;
 	}
 	return (0);
 }
