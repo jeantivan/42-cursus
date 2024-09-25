1a3e66e700e4e3dca9aea05a559b475607086471
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-25 18:52:01 +0200

    ADD: Si falla la reserva de memoría retorna NULL

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -20,6 +20,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 
 	len = ft_strlen(s);
 	new_s = (char *)malloc((len + 1) * sizeof(char));
+	if (!new_s)
+		return (NULL);
 	i = 0;
 	while (s[i] != '\0')
 	{
