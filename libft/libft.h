7670878de2d87cf0ef97ee1c048329f82ded6936
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-25 18:05:27 +0200

    ADD: Funciones ft_putendl_fd y ft_putnbr_fd estan listas

--- libft/libft.h
+++ libft/libft.h
@@ -49,14 +49,14 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_itoa(int n);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 /* Por hacer */
-void	ft_putendl_fd(char *s, int fd);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-void	ft_putnbr_fd(int n, int fd);
 
 /* TODO: Bonus functions */
 #endif /* libft.h */
