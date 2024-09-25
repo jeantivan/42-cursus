b61942b7e758240b9f03d2194be2405add4c59bd
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-25 17:10:12 +0200

    ADD: Finalizada la implementación de ft_strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -16,16 +16,17 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*new_str;
 	size_t	total_len;
-	size_t	s1_len;
-	size_t	s2_len;
 
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	total_len = s1_len + s2_len + 1;
-	new_str = (char *)malloc(total_len * sizeof(char));
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	total_len = ft_strlen(s1) + ft_strlen(s2);
+	new_str = (char *)malloc((total_len + 1) * sizeof(char));
 	if (!new_str)
 		return (NULL);
-	ft_strlcat(new_str, s1, total_len);
-	ft_strlcat(new_str, s2, total_len);
+	new_str[0] = '\0';
+	ft_strlcat(new_str, s1, total_len + 1);
+	ft_strlcat(new_str, s2, total_len + 1);
 	return (new_str);
 }
