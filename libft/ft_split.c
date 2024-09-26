612414d0b3fde6e044af1a861efb72a2b6c2425b
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-26 20:34:46 +0200

    WIP: Función casi finalizada

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdio.h>
 /**
  * Función ft_split
  *
@@ -31,10 +33,72 @@
  *
  * 1- Recorrer s y contar las apariciones del delimitador c -> (int n_of_c)
  * 2- Alocar la memoria para el array de string -> (n + 1) * sizeof(char *)
- * 3- Crear una variable char *temp_s que almacenará la s desplazandose cada vez que se encuentre el delimitador;
- * 4- Crear una función auxiliar que recorra s la cantidad de veces que aparezca el delimitador
+ * 3- Crear una variable char *temp_s que almacenará la s
+ *    desplazandose cada vez que se encuentre el delimitador;
+ * 4- Crear una función auxiliar que recorra s la cantidad de veces
+ *    que aparezca el delimitador
  */
+static int	ft_find_n_of_c(char const *s, char c)
+{
+	int	n;
+	int	i;
+
+	i = 0;
+	n = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
+			n++;
+		i++;
+	}
+	return (n);
+}
+
+static char	*ft_get_str_piece(char *temp_s, char c)
+{
+	int		len;
+	char	*piece;
+
+	len = 0;
+	while (temp_s[len] != c)
+		len++;
+	piece = ft_substr(temp_s, 0, len);
+	return (piece);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	int		n_of_c;
+	int		pace;
+	char	**arr;
+	char	*temp_s;
 
+	n_of_c = ft_find_n_of_c(s, c);
+	arr = (char **)malloc((n_of_c + 2) * sizeof(char *));
+	if (!arr)
+		return (NULL);
+	temp_s = (char *)s;
+	pace = 0;
+	while (pace <= n_of_c)
+	{
+		while (*temp_s != '\0')
+		{
+			if (temp_s[0] == c && temp_s[1] != c && temp_s[1] != '\0')
+			{
+				arr[pace] = ft_get_str_piece(temp_s + 1, c);
+				temp_s = temp_s + ft_strlen(arr[pace]) + 1;
+				break ;
+			}
+			else if (temp_s[0] != c && temp_s[1] != '\0')
+			{
+				arr[pace] = ft_get_str_piece(temp_s, c);
+				temp_s = temp_s + ft_strlen(arr[pace]);
+				break ;
+			}
+			temp_s++;
+		}
+		pace++;
+	}
+	arr[pace] = NULL;
+	return (arr);
 }
