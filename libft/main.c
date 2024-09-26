e38924c60651a1422d4b128bb7fbff7c22c12c42
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-26 20:34:21 +0200

    ADD: Testeando ft_split

--- libft/main.c
+++ libft/main.c
@@ -17,40 +17,117 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void	ft_test_striteri(unsigned int i, char *c)
+void	ft_print_result(char const *s)
 {
-	printf("index: %i, char: %c\n", i, *c);
+	int		len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	write(1, s, len);
 }
-
-char	ft_test_strmapi(unsigned int index, char c)
-{
-	if (index % 2 == 0 && ('a' <= c && c <= 'z'))
-	{
-		printf("index: %i, char: %c\n", index, c);
-		return (c - 32);
-	}
-	return ('-');
-};
-
 
 int		main(int argc, const char *argv[])
 {
-	int	arg;
-	alarm(5);
+	char	**tabstr;
+	int		i;
+	int		arg;
 
+	alarm(5);
 	if (argc == 1)
 		return (0);
+	i = 0;
 	if ((arg = atoi(argv[1])) == 1)
 	{
-		printf("-----Testing ft_striteri-----\n");
-		ft_striteri("Hola mundo!", ft_test_striteri);
+		if (!(tabstr = ft_split("          ", ' ')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
 	}
 	else if (arg == 2)
 	{
-		printf("-----Testing ft_strmapi-----\n");
-		printf("%s\n", ft_strmapi("Hola mundo!", ft_test_strmapi));
-
+		if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse", ' ')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
 	}
-
+	else if (arg == 3)
+	{
+		if (!(tabstr = ft_split("   lorem   ipsum dolor     sit amet, consectetur   adipiscing elit. Sed non risus. Suspendisse   ", ' ')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 4)
+	{
+		if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'i')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 5)
+	{
+		if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultricies diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.", 'z')))
+			ft_print_result("NULL");
+		else
+		{
+			while (tabstr[i] != NULL)
+			{
+				ft_print_result(tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+		}
+	}
+	else if (arg == 6)
+	{
+		if (!(tabstr = ft_split("", 'z')))
+			ft_print_result("NULL");
+		else
+			if (!tabstr[0])
+				ft_print_result("ok\n");
+	}
 	return (0);
 }
+
+/* int	main(void)
+{
+	int i;
+	char **arr = ft_split("***lorem***ipsum*dolor*****sit*amet,*consectetur***adipiscing*elit.*Sed*non*risus.*Suspendisse***",'*');
+
+	i = 0;
+	while (arr[i] != NULL)
+	{
+		//printf("i: %i ,arr[i]: %s\n", i, arr[i]);
+		i++;
+	}
+	return (0);
+} */
