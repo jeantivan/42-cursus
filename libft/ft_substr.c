cc499c77a5ecbf8492b9140524dcfc1deb78d81a
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-30 19:15:10 +0200

    CHANGE: Se toman en cuenta algunos edge cases

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 17:59:39 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/09/24 17:59:42 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/09/30 17:08:04 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,19 @@ char	*ft_substr(char const *s, unsigned int start,
 size_t len)
 {
 	char	*substr;
+	size_t	r_len;
+	size_t	s_len;
 
-	substr = (char *)malloc((len + 1) * sizeof(char));
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len)
+		r_len = s_len;
+	else
+		r_len = len;
+	substr = (char *)malloc((r_len + 1) * sizeof(char));
 	if (!substr)
 		return (NULL);
 	ft_strlcpy(substr, (s + start), len + 1);
