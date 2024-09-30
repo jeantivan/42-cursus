2dfcb568b720a0e29704d69242e7d40e3b25ad8c
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-30 19:14:10 +0200

    CHANGE: Pasa la norma pero tiene unos errores (WIP)

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,61 +6,56 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 12:00:15 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/09/29 12:54:02 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/09/30 17:48:59 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-char	*ft_strtrim(char const *s1, char const *set)
+static int	ft_find_start(char const *s1, char const *set)
 {
-	size_t	len;
-	char	*n_str;
-	size_t	n_str_len;
-	int		start;
-	int		i;
-	int		end;
+	int	start;
+	int	i;
 
-	len = ft_strlen(s1);
-	/* Paso 1. Encontrar start */
-	start = 0;
 	i = 0;
-	char *c_in_set;
-	//printf("** SET: %s\n\n", set);
-	//printf("** S1 LEN: %zu\n\n", len);
-
-	//printf("\n Paso 1. Encontrar start \n");
-	while (s1[i] != '\0')
+	while (s1[i])
 	{
-		c_in_set = ft_strchr(set, s1[i]);
-		//printf("---i: %i, s[i]: %c\n", i, s1[i]);
-		//printf("--set: %s, ft_strchr(set, s1[i]): %s\n", set,c_in_set);
-		if(!c_in_set)
+		if (!ft_strchr(set, s1[i]))
 			break ;
-		start = i +1;
+		start = i + 1;
 		i++;
 	}
-	//printf("Start: %i\n", start);
+	return (start);
+}
 
-	/* Paso 2: Encontrar end*/
-	//printf("\n Paso 2: Encontrar end \n");
-	i = len;
+static int	ft_find_end(char const *s1, char const *set)
+{
+	int	end;
+	int	i;
+
+	i = ft_strlen(s1);
 	while (i != 0)
 	{
-		c_in_set = ft_strchr(set, s1[i]);
-		//printf("---i: %i, s[i]: %c\n", i, s1[i]);
-		//printf("--set: %s, ft_strchr(set, s1[i]): %s\n", set,c_in_set);
-		if(!c_in_set)
+		if (!ft_strchr(set, s1[i]))
 			break ;
 		end = i;
 		i--;
 	}
-	//printf("End: %i\n", end);
-	n_str_len = end - start;
-	//printf("n_str_len: %zu\n", n_str_len);
+	return (end);
+}
 
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*n_str;
+	size_t	n_str_len;
+	int		start;
+	int		end;
+
+	if (!set)
+		return (ft_strdup(s1));
+	start = ft_find_start(s1, set);
+	end = ft_find_end(s1, set);
+	n_str_len = end - start;
 	n_str = ft_substr(s1, start, n_str_len);
-	//printf("New_str: %s\n", n_str);
 	return (n_str);
 }
