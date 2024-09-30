86ef3680ee7b3424d5ef0601c0ef09ed9d656505
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-09-30 19:16:16 +0200

    TEST: ft_strrchr

--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 17:48:08 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/09/29 12:54:36 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/09/30 19:10:05 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,19 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-
-
 int	main(void)
 {
-	const char str[] = "lorem \n ipsum \t dolor \n sit \t amet";
-	const char set[] = " ";
+	/* const char	str[] = " lorem ipsum dolor sit amet";
+	const char	set[] = "l "; */
 
-	printf("%s\n",ft_strtrim(str, set));
+/* 	printf("ft_strtrim(\"abcd\", \"\") = \"%s\"\n", ft_strtrim("abcd", ""));
+	printf("ft_strtrim(\"tripouille   xxx\", \" x\") = \"%s\"\n", ft_strtrim("tripouille   xxx", " x"));
+	printf("ft_substr(\"hola\", 0, 18446744073709551615) = \"%s\"\n", ft_substr("hola", 0, 184467440)); */
+
+	char s2[] = "xteste";
+
+//char s[] = "tripouille";
+	printf("%s\n", strrchr(s2, 'x'));
+	printf("%s\n", ft_strrchr(s2, 'x'));
 	return (0);
 }
