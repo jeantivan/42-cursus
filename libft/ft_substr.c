ee4f10b2be23c0d1bb8148cced404a61a1bd2556
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-01 20:03:54 +0200

    CHANGE: Calculo real de la memoria que se asigna

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,14 +6,13 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 17:59:39 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/09/30 17:08:04 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/01 19:51:01 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start,
-size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	r_len;
@@ -24,13 +23,12 @@ size_t len)
 	s_len = ft_strlen(s);
 	if (start >= s_len)
 		return (ft_strdup(""));
-	if (len > s_len)
-		r_len = s_len;
-	else
+	r_len = s_len - start;
+	if (len < r_len)
 		r_len = len;
 	substr = (char *)malloc((r_len + 1) * sizeof(char));
 	if (!substr)
 		return (NULL);
-	ft_strlcpy(substr, (s + start), len + 1);
+	ft_strlcpy(substr, (s + start), r_len + 1);
 	return (substr);
 }
