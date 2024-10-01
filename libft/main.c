6091f5f438b61852c74089c89664c7516c84f6ab
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-01 20:19:03 +0200

    WIP

--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 17:48:08 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/01 20:04:57 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/01 20:09:55 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,12 @@
 
 int	main(void)
 {
-	char	*s;
+	char	*haystack;
+	char	*needle;
 
-	s = ft_strtrim("tripouille   xxx", " x");
-	printf("test: %s, len:%zu \n", s, ft_strlen(s));
+	haystack = "aaabcabcd";
+	needle = "aabc";
+
+	printf("Test: %s\n", ft_strnstr(haystack, "aaabc", 5));
 	return (0);
 }
