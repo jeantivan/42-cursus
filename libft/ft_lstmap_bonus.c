db313a5107702d4706e50c040de4828aa7d72d44
Jean <jptivan53@gmail.com>
2024-10-01 23:53:04 +0200

    ADD: Libera la memoria del content del nodo si falla al crearse el nodo como tal

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 18:40:30 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/01 19:13:24 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/01 23:52:13 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*temp_node;
+	void	*content;
 
 	if (!lst)
 		return (NULL);
 	new_list = NULL;
 	while (lst)
 	{
-		temp_node = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		temp_node = ft_lstnew(content);
 		if (!temp_node)
 		{
+			del(content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
