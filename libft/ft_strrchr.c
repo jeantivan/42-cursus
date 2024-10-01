c1d2107eb10ea59445dca5f8acc0e2ba5e7165b6
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-01 20:03:21 +0200

    CHANGE: Solución a errores en paco

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 17:16:52 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/09/30 19:07:10 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/01 19:42:57 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,11 @@ char	*ft_strrchr(const char *s, int c)
 		return ((char *)(s + i));
 	while (i != 0)
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			return ((char *)(s + i));
 		i--;
 	}
+	if (i == 0 && s[i] == (char)c)
+		return ((char *)s);
 	return (NULL);
 }
