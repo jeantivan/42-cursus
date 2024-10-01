1708b94377b8b3f0fc75069a8ff9ec2b4789fa5c
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-01 16:52:46 +0200

    CREATE: Funcion ft_lstsize devuelve la cantidad de elementos de una lista t_list

--- libft/ft_lstsize_bonus.c
+++ libft/ft_lstsize_bonus.c
@@ -1,18 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 15:37:59 by jtivan-r          #+#    #+#             */
+/*   Updated: 2024/10/01 16:07:21 by jtivan-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 int	ft_lstsize(t_list *lst)
 {
-	int	size;
-	//t_list	temp_node;
-	//t_list	*curr_node;
+	int		size;
+	t_list	*curr_node;
 
 	size = 0;
-	/* curr_node->next = lst;
-	while(curr_node->next != NULL)
+	curr_node = lst;
+	while (curr_node != NULL)
 	{
-		temp_node = curr_node;
-		curr_node->next = temp_node.next;
+		curr_node = curr_node->next;
 		size++;
-	} */
+	}
 	return (size);
 }
