65740805443ad91a7314a4bc68896125318ba3b6
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-01 20:05:21 +0200

    CHANGE: Testing strtrim

--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 17:48:08 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/01 19:20:40 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/01 20:04:57 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,88 +16,11 @@
 #include <stdio.h>
 #include "libft.h"
 
-void	ft_print_result(t_list *elem)
+int	main(void)
 {
-	int		len;
+	char	*s;
 
-	len = 0;
-	while (((char *)elem->content)[len])
-		len++;
-	write(1, ((char *)elem->content), len);
-	write(1, "\n", 1);
-}
-
-t_list	*ft_lstnewone(void *content)
-{
-	t_list	*elem;
-
-	elem = (t_list *)malloc(sizeof(t_list));
-	if (!elem)
-		return (NULL);
-	if (!content)
-		elem->content = NULL;
-	else
-		elem->content = content;
-	elem->next = NULL;
-	return (elem);
-}
-
-void	*ft_map(void *ct)
-{
-	int i;
-	void	*c;
-	char	*pouet;
-
-	c = ct;
-	i = -1;
-	pouet = (char *)c;
-	while (pouet[++i])
-		if (pouet[i] == 'o')
-			pouet[i] = 'a';
-	return (c);
-}
-
-void    ft_del(void *content)
-{
-	free(content);
-}
-
-int main(int argc, const char *argv[])
-{
-	t_list		*elem;
-	t_list		*elem2;
-	t_list		*elem3;
-	t_list		*elem4;
-	t_list		*list;
-	char		*str = strdup("lorem");
-	char		*str2 = strdup("ipsum");
-	char		*str3 = strdup("dolor");
-	char		*str4 = strdup("sit");
-
-	elem = ft_lstnewone(str);
-	elem2 = ft_lstnewone(str2);
-	elem3 = ft_lstnewone(str3);
-	elem4 = ft_lstnewone(str4);
-	if (argc == 1 || !elem || !elem2 || !elem3 || !elem4)
-		return (0);
-	elem->next = elem2;
-	elem2->next = elem3;
-	elem3->next = elem4;
-	if (atoi(argv[1]) == 1)
-	{
-		if (!(list = ft_lstmap(elem, &ft_map, &ft_del)))
-			return (0);
-		if (list == elem)
-			write(1, "A new list is not returned\n", 27);
-		int i;
-		i = 0;
-		ft_print_result(list);
-		while (list->next)
-		{
-			list = list->next;
-			ft_print_result(list);
-			i++;
-		}
-	}
+	s = ft_strtrim("tripouille   xxx", " x");
+	printf("test: %s, len:%zu \n", s, ft_strlen(s));
 	return (0);
 }
