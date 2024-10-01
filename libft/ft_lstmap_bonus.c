ef6291fc076c044ca270338511ecbe9bda0da529
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-01 19:17:46 +0200

    CREATE: Función ft_lstmap crea una nueva lista con el resultado de aplicarle una función f a cada node->content de la lista original

---
+++ libft/ft_lstmap_bonus.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 18:40:30 by jtivan-r          #+#    #+#             */
+/*   Updated: 2024/10/01 19:13:24 by jtivan-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*temp_node;
+
+	if (!lst)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		temp_node = ft_lstnew(f(lst->content));
+		if (!temp_node)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, temp_node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
