58c130374eb376a04e9ea99da075c486a411121b
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-02 17:49:53 +0200

    Primer commit del proyecto

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 17:14:53 by jtivan-r          #+#    #+#             */
+/*   Updated: 2024/10/02 17:49:03 by jtivan-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
+
+char	*get_next_line(void);
+
+#endif /* get_next_line.h */
---
+++ get_next_line/main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 17:33:00 by jtivan-r          #+#    #+#             */
+/*   Updated: 2024/10/02 17:49:11 by jtivan-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	get_next_line();
+	get_next_line();
+	get_next_line();
+	get_next_line();
+	get_next_line();
+	return (0);
+}
