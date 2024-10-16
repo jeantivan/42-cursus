86d9e94f857e7f9f2ab279e54bd044670c547178
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-16 16:26:34 +0200

    ADD: Util para imprimir un numero en hexadecimal

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,16 +6,16 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:20:33 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/16 15:38:03 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/16 16:26:03 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# include <unistd.h>
 # include <stdarg.h>
+# include <unistd.h>
 
 int	ft_printf(char const *s, ...);
-
+int	ft_nbr_to_hex(int nbr, int uppercase);
 #endif /* ft_printf.h */
