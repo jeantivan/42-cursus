537c8f7f081f029ea239c104966e52626e068cee
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-16 19:50:20 +0200

    WIP

--- ft_printf/ft_nbr_to_hex.c
+++ ft_printf/ft_nbr_to_hex.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 16:19:00 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/16 16:22:07 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/16 18:41:30 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,13 @@
 
 int	ft_nbr_to_hex(int nbr, int upper)
 {
+	char	*letters;
+
+	if (upper <= 0)
+		letters = "0123456789abcdef";
+	else
+		letters = "0123456789ABCDEF";
+	if (nbr == 0)
+		return (0);
+	return (1);
 }
