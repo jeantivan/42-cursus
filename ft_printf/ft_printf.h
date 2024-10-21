903672e687fcfc932ab4fc0549932251d9a9ba76
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-21 19:25:20 +0200

    ADD: Funciones utilitarias

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:20:33 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/16 19:06:46 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/21 18:42:53 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_printf(char const *s, ...);
-int	ft_nbr_to_hex(int nbr, int uppercase);
+int		ft_printf(char const *s, ...);
+char	*ft_nbr_to_hex(int nbr, int uppercase);
+/* Utils */
+int		ft_putchar(char c);
+int		ft_putstr(char *s);
+int		ft_putnbr(int nbr);
 #endif /* ft_printf.h */
