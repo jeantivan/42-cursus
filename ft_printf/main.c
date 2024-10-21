7fb882d3fa3e3038802e73ebe6a9d0c1d46e3a29
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-21 19:28:50 +0200

    Probando el trabajo hecho

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,11 +6,19 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:39:36 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/16 19:10:55 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/21 19:22:08 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+#include "libft/libft.h"
+#include <stdio.h>
+
 int	main(void)
 {
+	int	counter;
+
+	counter = ft_printf("Hola %% asfas");
+	printf("\nPrinted %i chars \n", counter);
 	return (0);
 }
