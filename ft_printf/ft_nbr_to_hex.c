5b425207aa6e5302feb01a0d06237baf1bf65df3
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-21 19:25:03 +0200

    ADD: Convierte un numero entero positivo a hexadecimal

--- ft_printf/ft_nbr_to_hex.c
+++ ft_printf/ft_nbr_to_hex.c
@@ -6,21 +6,57 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 16:19:00 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/17 20:20:18 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/21 18:41:46 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "libft/libft.h"
 
-int	ft_nbr_to_hex(int nbr, int upper)
+char	*ft_reverse_string(char *s)
+{
+	int		i;
+	int		len;
+	char	temp_letter;
+
+	// char	*new_s;
+	i = 0;
+	len = ft_strlen(s);
+	while (i < (len / 2))
+	{
+		temp_letter = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp_letter;
+		i++;
+	}
+	return (s);
+}
+// TODO: convertir los números negativos también
+// NO TENGO NPI de como hacerlo
+char	*ft_nbr_to_hex(int nbr, int upper)
 {
 	char	*letters;
+	char	*hex;
+	int		i;
 
+	i = 0;
+	hex = (char *)malloc(sizeof(char) * 16 + 1);
+	if (!hex)
+		return (NULL);
 	if (upper <= 0)
 		letters = "0123456789abcdef";
 	else
 		letters = "0123456789ABCDEF";
 	if (nbr == 0)
-		return (0);
-	return (1);
+		hex[i] = '0';
+	else
+	{
+		while (nbr > 0)
+		{
+			hex[i++] = letters[nbr % 16];
+			nbr /= 16;
+		}
+		ft_reverse_string(hex);
+	}
+	return (hex);
 }
