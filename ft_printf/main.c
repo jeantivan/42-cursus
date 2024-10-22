2e19186ccc73c43b8ab2ccc124f252a08cbef5a5
Jean <jptivan53@gmail.com>
2024-10-22 22:06:13 +0200

    CHANGE: Creo que ya, esta listo!

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,18 +6,19 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:39:36 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/22 19:14:32 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/22 21:57:53 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 #include "libft/libft.h"
+#include <limits.h>
 
 int	main(void)
 {
 	int	counter;
-	int	x;
+	/* int	x;
 	unsigned int ux;
 
 	x = -4242;
@@ -25,6 +26,11 @@ int	main(void)
 	counter = ft_printf("Testing my own \n Porcentaje %%, c: %c seguimos, d: %d, i: %i, u: %u, p: %p, x: %x, X: %X", -128, 42, -402021, ux, &x, x, x);
 	printf("\nPrinted %i chars \n", counter);
 	counter = printf("Testing my own \n Porcentaje %%, c: %c seguimos, d: %d, i: %i, u: %u, p: %p, x: %x, X: %X", -128, 42, -402021, ux, &x, x, x);
+	printf("\nPrinted %i chars \n", counter); */
+	counter = ft_printf(" %i ", -1);
 	printf("\nPrinted %i chars \n", counter);
+
+	printf("\nPrinted %i chars \n", printf(" %i ", INT_MIN));
+
 	return (0);
 }
