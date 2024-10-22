1b9f5c4efad7cffcd8668f7d87304597ea4e0aba
Jean <jptivan53@gmail.com>
2024-10-22 22:08:10 +0200

    CHANGE: Manejo de los edge cases 0 INT_MIN INT_MAX LONG_MIN

--- ft_printf/ft_putnbr.c
+++ ft_printf/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 18:43:33 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/22 16:38:27 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/22 22:07:03 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,24 @@ static int	ft_get_digits(unsigned int nbr)
 
 int	ft_putnbr(int nbr)
 {
-	int	tot;
-	int	digits;
+	int			tot;
+	int			digits;
+	long int	temp_nbr;
 
 	tot = 0;
+	if (nbr == 0)
+		return (ft_putchar('0'));
+	temp_nbr = nbr + 0;
 	if (nbr < 0)
 	{
 		tot += ft_putchar('-');
-		nbr = nbr * -1;
+		temp_nbr = temp_nbr * -1;
 	}
-	digits = ft_get_digits(nbr);
+	digits = ft_get_digits(temp_nbr);
 	while (digits != 0)
 	{
-		tot += ft_putchar((nbr / digits) + '0');
-		nbr = nbr % digits;
+		tot += ft_putchar((temp_nbr / digits) + '0');
+		temp_nbr = temp_nbr % digits;
 		digits = digits / 10;
 	}
 	return (tot);
@@ -45,15 +49,19 @@ int	ft_putnbr(int nbr)
 
 int	ft_putnbr_uns(unsigned int nbr)
 {
-	int	tot;
-	int	digits;
+	int					tot;
+	int					digits;
+	unsigned long int	temp_nbr;
 
 	tot = 0;
+	temp_nbr = nbr + 0;
 	digits = ft_get_digits(nbr);
+	if (nbr == 0)
+		return (ft_putchar('0'));
 	while (digits != 0)
 	{
-		tot += ft_putchar((nbr / digits) + '0');
-		nbr = nbr % digits;
+		tot += ft_putchar((temp_nbr / digits) + '0');
+		temp_nbr = temp_nbr % digits;
 		digits = digits / 10;
 	}
 	return (tot);
