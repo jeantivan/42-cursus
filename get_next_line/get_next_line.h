81f2913afe4dd106b39dde326bb36e7936f49127
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-23 17:35:31 +0200

    WIP: Probando la compilación con diferentes buffer_size

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,16 +6,19 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 17:14:53 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/03 11:36:57 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/23 17:10:42 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 1024
 
 # include <fcntl.h>
 # include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1023
+# endif
 
 char	*get_next_line(int fd);
 void	ft_putchar(char c);
--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 17:33:00 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/03 12:00:57 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/23 16:53:58 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int	main(void)
 	fd = open("./test/test01", O_RDONLY);
 	if (fd < 0)
 		return (1);
+	printf("Buffer Size: %i\n", BUFFER_SIZE);
 	get_next_line(fd);
 	return (0);
 }
