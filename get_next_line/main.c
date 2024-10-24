149416e33e9fed23627aac845fa63ae91b140d14
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-24 19:05:12 +0200

    TESTING :D

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,21 +6,33 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 17:33:00 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/10/23 16:53:58 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/10/24 18:54:47 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
+
 
 int	main(void)
 {
 	int	fd;
+	//int lines;
 
+	//lines = 0;
 	fd = open("./test/test01", O_RDONLY);
 	if (fd < 0)
 		return (1);
-	printf("Buffer Size: %i\n", BUFFER_SIZE);
+	/* while(lines <= 15)
+	{
+		if (lines % 2 == 0)
+			printf(ANSI_COLOR_GREEN"%s"ANSI_COLOR_RESET, get_next_line(fd));
+		else
+			printf(ANSI_COLOR_MAGENTA"%s"ANSI_COLOR_RESET, get_next_line(fd));
+		lines++;
+	} */
 	get_next_line(fd);
+	get_next_line(fd);
+	get_next_line(fd);
+	close(fd);
 	return (0);
 }
