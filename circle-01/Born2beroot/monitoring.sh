39a093b8b7125e60721c59c27ea1931e0b5ad277
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-10-31 19:28:15 +0100

    Cambios en algunos comandos del script

--- circle-01/Born2beroot/monitoring.sh
+++ circle-01/Born2beroot/monitoring.sh
@@ -4,28 +4,28 @@
 ARC=$(uname -v)
 
 #CPU Physical
-CPU=$(grep "physical id" /proc/cpuinfo | wc -l)
+CPU=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
 
 #CPU Virtual
-vCPU=$(grep processor /proc/cpuinfo | wc -l)
+vCPU=$(grep "^processor" /proc/cpuinfo | wc -l)
 
 # USED RAM 
 RAM_USED=$(free --mega | awk '$1 == "Mem:" {print $3}')
 
 #TOTAL RAM
-RAM_TOT=$(free --mega | awk '$1 == "Mem:" {printf("%dMB"), $2}')
+RAM_TOT=$(free --mega | awk '$1 == "Mem:" {printf("%d"), $2}')
 
 # % RAM used
 POR_RAM_USED=$(free --mega | awk '$1 == "Mem:" {printf("%.2f%%"),  $3/$2*100}')
 
-# Disk Used
-DISK_USED=$(df -m | grep "/dev" | grep -v "/boot" | awk '{disk_use += $3} END {print disk_use}')
+# DISK TOTAL
+DISK_TOT=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{dt += $2} END {printf ("%.1fGb\n"), dt/1024}')
 
-# Disk Total
-DISK_TOT=$(df -m | grep "/dev" | grep -v "/boot" | awk '{disk_tot += $2} END {printf("%.0fGb"), disk_tot/1024}')
+#DISK_USED
+DISK_USED=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{du += $3} END {print du}')
 
-# % Disk used
-POR_DISK_USED=$(df -m | grep "/dev" | grep -v "/boot" | awk '{used += $3} {tot += $2} END {printf("%d%%"), used/tot*100}')
+#PERCENT DISK USED
+POR_DISK_USED=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{du += $3} {dt+= $2} END {printf("%d"), du/dt*100}')
 
 # % CPU Used
 CPU_LOAD=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
@@ -51,11 +51,11 @@ MAC_ADDRESS=$(ip link | grep "link/ether" | awk '{print $2}')
 # Num command eject with sudo
 N_SUDO_COMMAND=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
 
-wall -n "       #Architecture: $ARC
+wall -n "        #Architecture: $ARC
         #CPU (physical): $CPU        
         #vCPU: $vCPU
-        #Memory Usage: $RAM_USED/$RAM_TOT ($POR_RAM_USED)
-        #Disk Usage: $DISK_USED/$DISK_TOT ($POR_DISK_USED)
+        #Memory Usage: $RAM_USED/${RAM_TOT}MB ($POR_RAM_USED)
+        #Disk Usage: $DISK_USED/${DISK_TOT} ($POR_DISK_USED%)
         #CPU Load: $CPU_LOAD
         #Last boot: $LAST_REBOOT
         #LVM use: $USE_LVM
