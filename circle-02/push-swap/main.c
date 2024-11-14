4e6ad05fa4d387356611b801cce020a1d688ea1a
Jean <jptivan53@gmail.com>
2024-11-14 22:28:13 +0100

    WIP: Terminado el parseo

--- circle-02/push-swap/main.c
+++ circle-02/push-swap/main.c
@@ -6,62 +6,28 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:29:05 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/11/13 18:05:29 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/11/14 22:21:25 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "error.h"
 #include "ft_printf/ft_printf.h"
 #include "libft/libft.h"
-#include "list.h"
+#include "stack.h"
 #include "parser.h"
 #include "utils.h"
 #include <stdio.h>
 
-int	main(void)
+int main(int argc, char **argv)
 {
-	Stack	*stack;
-	t_list	*temp;
-
-	stack = create_stack();
-	ft_printf("Stack size: %i\n", stack->size);
-	add_to_stack(stack, new_node(12));
-	temp = stack->head;
-	while (temp)
-	{
-		printf("%i\n", *((int *)temp->content));
-		temp = temp->next;
-	}
-	ft_printf("Stack size: %i\n", stack->size);
-	return (0);
-}
-
-/* int main(int argc, char **argv)
-{
-	int	i;
-	t_list	*values;
-	//char	**input;
-	size_t	count;
+	t_stack	*stack_a;
 
 	if (argc <= 1)
 		return (print_error());
-
-	i = 1;
-	while (argv[i])
-	{
-		if (!valid_input(argv[i]))
-		{
-			ft_printf("Invalid input %s\n", argv[i]);
-			return (print_error());
-		}
-		i++;
-	}
-	count = count_elements(argv);
-	printf("N of elements: %zu\n", count);
-	values = parse_values(argv);
-	if (!values)
+	stack_a = parse_values(argv, argc);
+	if(!stack_a)
 		return (print_error());
-	print_list(values);
-	ft_lstclear(&values, free);
+	show_stack(stack_a);
+	free_stack(stack_a);
 	return (0);
-} */
+}
