0a2bb9275b46f003ae06a233f47cfbeb020986fe
Jean <jptivan53@gmail.com>
2024-11-14 22:26:52 +0100

    ADD: Header para poder trabajar con los stack. Solo hay una función a "exportar"

--- circle-02/push-swap/parser.h
+++ circle-02/push-swap/parser.h
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 18:46:59 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/11/13 18:13:06 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/11/14 21:44:25 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 # define PARSER_H
 
 # include "libft/libft.h"
+# include "stack.h"
 
-int		valid_el(char *el);
-t_list	*parse_values(char **argv, int argc);
-int		valid_input(char *input);
+t_stack	*parse_values(char **argv, int argc);
 
 #endif /* parser.h */
