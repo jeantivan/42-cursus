d551418e103ad1536dd832a8ca674f04927a2dd3
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-11-15 12:59:19 +0100

    ADD: Ultimos test de rotate y reverse_r

--- circle-02/push-swap/main.c
+++ circle-02/push-swap/main.c
@@ -6,17 +6,17 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:29:05 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/11/14 23:41:23 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/11/15 12:53:46 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "error.h"
 #include "ft_printf/ft_printf.h"
 #include "libft/libft.h"
-#include "stack.h"
-#include "parser.h"
-#include "utils.h"
 #include "operations.h"
+#include "parser.h"
+#include "stack.h"
+#include "utils.h"
 #include <stdio.h>
 
 void	test_swap(t_stack *stack)
@@ -34,30 +34,56 @@ void	test_swap(t_stack *stack)
 
 void	test_push(t_stack *stack_1, t_stack *stack_2)
 {
-	ft_printf("---- Testing swap -----\n");
+	ft_printf("\n---- Testing push -----\n");
 	show_stack(stack_1);
 	show_stack(stack_2);
-	ft_printf("--- Pushing two times from stack_a to stack_b\n");
+	ft_printf("\n--- Pushing two times from stack_a to stack_b\n");
 	push(stack_1, stack_2);
 	push(stack_1, stack_2);
 	show_stack(stack_1);
 	show_stack(stack_2);
-	ft_printf("--- Pushing two times from stack_b to stack_a\n");
+	ft_printf("\n--- Pushing two times from stack_b to stack_a\n");
 	push(stack_2, stack_1);
 	push(stack_2, stack_1);
 	show_stack(stack_1);
 	show_stack(stack_2);
-	ft_printf("--- Pushing one more time from stack_b to stack_a\n");
+	ft_printf("\n--- Pushing one more time from stack_b to stack_a\n");
 	push(stack_2, stack_1);
 	show_stack(stack_1);
 	show_stack(stack_2);
-	ft_printf("--- Pushing one more time from stack_b to stack_a\n");
+	ft_printf("\n--- Pushing one more time from stack_b to stack_a\n");
 	push(stack_2, stack_1);
 	show_stack(stack_1);
 	show_stack(stack_2);
 }
 
-int main(int argc, char **argv)
+void	test_rotate(t_stack *stack)
+{
+	ft_printf("\n---- Testing rotate -----\n");
+	show_stack(stack);
+	ft_printf("\n--- Rotating one time the stack\n");
+	rotate(stack);
+	show_stack(stack);
+	ft_printf("\n--- Rotating two times more the stack\n");
+	rotate(stack);
+	rotate(stack);
+	show_stack(stack);
+}
+
+void	test_reverse_r(t_stack *stack)
+{
+	ft_printf("\n---- Testing reverse rotate -----\n");
+	show_stack(stack);
+	ft_printf("\n--- Reverse rotate one time the stack\n");
+	reverse_r(stack);
+	show_stack(stack);
+	ft_printf("\n--- Reverse rotate two times more the stack\n");
+	reverse_r(stack);
+	reverse_r(stack);
+	show_stack(stack);
+}
+
+int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
@@ -66,7 +92,7 @@ int main(int argc, char **argv)
 		return (print_error());
 	stack_a = parse_values(argv, argc);
 	stack_b = create_stack();
-	if(!stack_a)
+	if (!stack_a)
 		return (print_error());
 	if (!stack_b)
 	{
@@ -75,7 +101,8 @@ int main(int argc, char **argv)
 	}
 	test_swap(stack_a);
 	test_push(stack_a, stack_b);
-
+	test_rotate(stack_a);
+	test_reverse_r(stack_a);
 	free_stack(stack_a);
 	free_stack(stack_b);
 	return (0);
