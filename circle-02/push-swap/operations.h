bcaf3a0cc31ccd7a459eda2fd854ec4536ba5f05
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2024-11-15 12:57:34 +0100

    CHANGE: Prototipo correcto

--- circle-02/push-swap/operations.h
+++ circle-02/push-swap/operations.h
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 22:30:25 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/11/14 22:35:41 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/11/15 12:42:57 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 # include "libft/libft.h"
 # include "stack.h"
 
-void  swap(t_stack *stack);
-void  push(t_stack *stack_1, t_stack *stack_2);
-void  rotate(t_stack *stack);
-void  reverse(t_stack *stack);
+void	swap(t_stack *stack);
+void	push(t_stack *stack_1, t_stack *stack_2);
+void	rotate(t_stack *stack);
+void	reverse_r(t_stack *stack);
 
 #endif /* operations.h */
