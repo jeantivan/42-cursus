0b890812ff00b80dfa8e6c220248d99a10a4b838
Jean <jptivan53@gmail.com>
2024-12-04 20:09:11 +0100

    CHANGE: Se usa small_sort

--- circle-02/push-swap/sort.c
+++ circle-02/push-swap/sort.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 22:46:27 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/12/04 17:57:22 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/12/04 19:02:02 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,8 @@ int	sort(t_stack *stack_a)
 		operations += sort_stack_2(stack_a, 'a');
 	else if (stack_a->size == 3)
 		operations += sort_stack_3(stack_a, 'a');
-	else if (stack_a->size == 4)
-		operations += sort_stack_4(stack_a, stack_b);
-	else if (stack_a->size == 5)
-		operations += sort_stack_5(stack_a, stack_b);
+	else if (stack_a->size > 3 && stack_a->size <= 5)
+		operations += small_sort(stack_a, stack_b);
 	else
 		operations = k_sort(stack_a, stack_b);
 	free_stack(stack_b);
