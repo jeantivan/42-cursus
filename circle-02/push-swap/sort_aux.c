fad362949fdaaacb4a4e65b25a82985e7c5cbb29
Jean <jptivan53@gmail.com>
2024-12-04 17:56:43 +0100

    ADD: Algoritmos auxiliares para ordenar stacks de 2 y 3 elementos

---
+++ circle-02/push-swap/sort_aux.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_aux.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 17:36:56 by jtivan-r          #+#    #+#             */
+/*   Updated: 2024/12/04 17:51:45 by jtivan-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	sort_stack_2(t_stack *stack, char letter)
+{
+	if (letter != 'a' && letter != 'b')
+		exit(EXIT_FAILURE);
+	if (peek(stack->head) > peek(stack->head->next))
+		return (sx(stack, letter));
+	return (0);
+}
+
+int	sort_stack_3(t_stack *stack, char letter)
+{
+	int		op;
+	int		n1;
+	int		n2;
+	int		n3;
+
+	if ((letter != 'a' && letter != 'b'))
+		exit(EXIT_FAILURE);
+	if (!stack->head || stack->size < 3)
+		return (-1);
+	op = 0;
+	n1 = peek(stack->head);
+	n2 = peek(stack->head->next);
+	n3 = peek(stack->head->next->next);
+	if (n1 > n2 && n1 > n3)
+		op += rx(stack, letter);
+	else if (n1 < n2 && n3 < n2)
+		op += rrx(stack, letter);
+	if (peek(stack->head) > peek(stack->head->next))
+		op += sx(stack, letter);
+	return (op);
+}
