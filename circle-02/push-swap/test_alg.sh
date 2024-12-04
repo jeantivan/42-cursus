3bb16e835ca1bcfa9e8adbc8e53a907b8fab7810
Jean <jptivan53@gmail.com>
2024-12-04 22:29:05 +0100

    CHANGE: Retocando el script

--- circle-02/push-swap/test_alg.sh
+++ circle-02/push-swap/test_alg.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-test_cases=("3")
-test_n="10"
+test_cases=("5" "10" "100" "500")
+test_n="20"
+
+BACKGROUND="\033[44;37m"
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+NC="\033[0m" 
+
+printf "\n        push_swap_tester      \n"
 
 i=0
-
 for test_case in "${test_cases[@]}"
 do
-    echo "n=$test_case "
+    printf "\n${BACKGROUND}      Testing with $test_case args          ${NC}\n"
+    printf "\n"
     i=0
     while [ $i -lt $test_n ]
     do
         args=$(shuf -i 0-50 -n $test_case | tr '\n' ' ')
-        echo "Args $args Checker: $(./push_swap $args | ./checker_linux $args)"
-        echo "$(./push_swap $args |  tr '\n' ' ')"
-        echo "Tot_mov $(./push_swap $args | wc -l)"
+        checker=$(./push_swap $args | ./checker_linux $args)
+        res="Checker: $checker | Mov: [$(./push_swap $args | wc -l)]"
+        if [ "$checker" == "OK" ]; then
+            echo -n -e "${GREEN}${res}${NC}"
+        else
+            echo -n -e "${RED}${res}${NC}"
+        fi
         echo ""
         ((i++))
     done
-    echo ""
 done
