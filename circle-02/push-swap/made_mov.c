a1314b67393daf05a7ca24951abc7be807dcb3c2
Jean <jptivan53@gmail.com>
2024-12-06 10:59:11 +0100

    CHANGE: Ejecutando los movimientos recibidos

--- circle-02/push-swap/made_mov.c
+++ circle-02/push-swap/made_mov.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 18:27:08 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/12/05 19:28:53 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/12/05 21:58:35 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	handle_swap(t_stack *stack_a, t_stack *stack_b, char *mov)
 	int	op;
 
 	op = 0;
-	if (ft_strncmp(mov, "sa\n", 5))
-		op += sx(stack_a, 'a', true);
-	else if (ft_strncmp(mov, "sb\n", 5))
-		op += sx(stack_b, 'b', true);
+	if (ft_strncmp(mov, "sa\n", 5) == 0)
+		op += sx(stack_a, 'a', false);
+	else if (ft_strncmp(mov, "sb\n", 5) == 0)
+		op += sx(stack_b, 'b', false);
 	else
-		op += ss(stack_a, stack_b, true);
+		op += ss(stack_a, stack_b, false);
 	return (op);
 }
 
@@ -31,10 +31,10 @@ int	handle_push(t_stack *stack_a, t_stack *stack_b, char *mov)
 	int	op;
 
 	op = 0;
-	if (ft_strncmp(mov, "pa\n", 5))
-		op += px(stack_a, stack_b, 'a', true);
-	else if (ft_strncmp(mov, "pb\n", 5))
-		op += px(stack_a, stack_b, 'b', true);
+	if (ft_strncmp(mov, "pa\n", 5) == 0)
+		op += px(stack_a, stack_b, 'a', false);
+	else if (ft_strncmp(mov, "pb\n", 5) == 0)
+		op += px(stack_a, stack_b, 'b', false);
 	return (op);
 }
 
@@ -43,32 +43,18 @@ int	handle_rotate(t_stack *stack_a, t_stack *stack_b, char *mov)
 	int	op;
 
 	op = 0;
-	if (ft_strncmp(mov, "ra\n", 5))
-	{
-		ft_printf("Hola 1 %c\n", ft_strncmp(mov, "ra\n", 5));
-		op += rx(stack_a, 'a', true);
-	}
-	else if (ft_strncmp(mov, "rb\n", 5))
-	{
-
-		op += rx(stack_b, 'b', true);
-	}
-	else if (ft_strncmp(mov, "rr\n", 5))
-	{
-		op += rr(stack_a, stack_b, true);
-	}
-	else if (ft_strncmp(mov, "rra\n", 5))
-	{
-		op += rrx(stack_a, 'a', true);
-	}
-	else if (ft_strncmp(mov, "rrb\n", 5))
-	{
-		op += rrx(stack_b, 'b', true);
-	}
+	if (ft_strncmp(mov, "ra\n", 5) == 0)
+		op += rx(stack_a, 'a', false);
+	else if (ft_strncmp(mov, "rb\n", 5) == 0)
+		op += rx(stack_b, 'b', false);
+	else if (ft_strncmp(mov, "rr\n", 5) == 0)
+		op += rr(stack_a, stack_b, false);
+	else if (ft_strncmp(mov, "rra\n", 5) == 0)
+		op += rrx(stack_a, 'a', false);
+	else if (ft_strncmp(mov, "rrb\n", 5) == 0)
+		op += rrx(stack_b, 'b', false);
 	else
-	{
-
-	}
+		op += rrr(stack_a, stack_b, false);
 	return (op);
 }
 
