3065fc1e35bc9dcafc0fe072707f76c27d82e749
Jean <jptivan53@gmail.com>
2024-12-12 00:01:46 +0100

    WIP: PRobando cositas

--- circle-02/FdF/main.c
+++ circle-02/FdF/main.c
@@ -6,20 +6,37 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 16:36:52 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/12/09 18:18:33 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2024/12/11 23:58:09 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include <stdio.h>
+#include <time.h>
 
-int	main(void)
+int	main(int ac, char **av)
 {
+	char	*file;
 	int		fd;
-	char	*line;
-	char	*line_cpy;
 
-	fd = open("./test2.txt", O_RDONLY);
+	if (ac != 2)
+		return (1);
+	file = av[1];
+	fd = open(file, O_RDONLY);
 	if (fd < 0)
-		perror("Error al intentar abrir el archivo");
+		ft_error("Bad file");
+
+	clock_t inicio, fin;
+	double tiempo_usado;
+
+	inicio = clock();
+	bool is_valid_map = valid_map(fd);
+	ft_printf("Valid map: %i\n", is_valid_map);
+	fin = clock();
+    // Calcular el tiempo de ejecución
+    tiempo_usado = ((double)(fin - inicio)) / CLOCKS_PER_SEC;
+    printf("Chequear el mapa tardó: %.8f segundos\n", tiempo_usado);
+
+	close(fd);
 	return (0);
 }
