846bc8d8a31270c84e2f01acf2655a26f6af2ed6
Jean <jptivan53@gmail.com>
2025-01-17 20:42:19 +0100

    CHANGE: Nuevos prototipos de funciones. Limpieza de codigo y cumplimiento de la norma

--- circle-02/FdF/fdf.h
+++ circle-02/FdF/fdf.h
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:59:29 by jtivan-r          #+#    #+#             */
-/*   Updated: 2025/01/16 18:59:53 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2025/01/17 20:40:25 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 # include <stdbool.h>
 # include <math.h>
 # ifndef M_PI
-#  define M_PI (3.14159265358979323846)
+#  define M_PI 3.14159265358979323846
 # endif
 # ifndef WIN_W
 #  define WIN_W 1280
@@ -35,22 +35,6 @@
 # define X 0
 # define Y 1
 # define Z 2
-
-typedef struct s_screen_config
-{
-	int		width;
-	int		height;
-	int		total_points;
-	double	padding;
-}	t_screen_config;
-
-typedef struct s_projection_offset
-{
-	double	x_offset;
-	double	y_offset;
-	double	spacing;
-}	t_projection_offset;
-
 typedef struct s_point
 {
 	float		coords[3];
@@ -70,13 +54,13 @@ typedef struct s_map
 	float		ang[3];
 }				t_map;
 
-typedef struct s_meta
+typedef struct s_neighbors
 {
-	t_map		*map;
-	float		*x_center;
-	float		*y_center;
-	float		spacing;
-}	t_meta;
+	t_point	*right;
+	t_point	*bottom;
+	bool	has_right;
+	bool	has_bottom;
+} t_neighbors;
 
 /* Error */
 void		ft_error(char *error_message);
@@ -98,26 +82,29 @@ void		get_points(int fd, t_map *map);
 void		ft_show_point(t_point point);
 
 /* Matrix */
-void	matrix_init(float (*matrix)[3]);
-t_point	matmul(float matrix[3][3], t_point point);
-t_point	rotate_x(t_point point, float ang);
-t_point	rotate_y(t_point point, float ang);
-t_point	rotate_z(t_point point, float ang);
+void		matrix_init(float (*matrix)[3]);
+t_point		matmul(float matrix[3][3], t_point point);
+t_point		rotate_x(t_point point, float ang);
+t_point		rotate_y(t_point point, float ang);
+t_point		rotate_z(t_point point, float ang);
 
 /* Proyection */
-void	get_center_coords(t_point *points, size_t len, float center[2]);
-void	ortho_proyection(t_point *points, size_t len);
-void	scale_points(t_point *points, size_t len, float factor);
-void	translate_points(t_point *points, size_t len, float move[2]);
-void	proyect_points(t_point *points, t_point *proyection,size_t len, float ang[3]);
+void		get_center_coords(t_point *points, size_t len, float center[2]);
+void		ortho_proyection(t_point *points, size_t len);
+void		scale_points(t_point *points, size_t len, float factor);
+void		translate_points(t_point *points, size_t len, float move[2]);
+void		proyect_points(t_point *points, t_point *proyection,\
+ size_t len, float ang[3]);
 
 /* Drawing*/
-void		drawline(mlx_image_t *image, t_point p1, t_point p2, uint32_t color);
+void		put_pixel(mlx_image_t *image, int x, int y, uint32_t color);
+void		dda(mlx_image_t *image, t_point point0, t_point point1);
 void		join_points(mlx_image_t *image, t_map *map);
+void		draw_dot(mlx_image_t *image, t_point p);
 
-/* Bresenham */
-void	plotLine(mlx_image_t *image, t_point point0, t_point point1);
+/* Draw utils */
+int			get_index(int x, int y, int cols);
+int			is_valid_point(int x, int y, int rows, int cols);
+t_neighbors	get_neighbors(int x, int y, t_map *map);
 
-/* DDA */
-void DDA(mlx_image_t *image, t_point point0, t_point point1);
 #endif /* fdf.h */
