657af39ba05fb24d2909e67af71b3a0c9ba40b5c
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2025-01-21 18:58:38 +0100

    ADD: Hook de limpieza al cerrar la ventana del programa

--- circle-02/FdF/hooks.c
+++ circle-02/FdF/hooks.c
@@ -6,21 +6,32 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 18:45:20 by jtivan-r          #+#    #+#             */
-/*   Updated: 2025/01/20 18:48:25 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2025/01/21 18:43:18 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	close_hook(mlx_key_data_t keydata, void *param)
+void	close_hook(void *param)
+{
+	t_state	*state;
+
+	state = (t_state *)param;
+	if (state->image)
+		mlx_delete_image(state->mlx, state->image);
+	clean_map(state->map);
+	mlx_close_window(state->mlx);
+	mlx_terminate(state->mlx);
+	ft_safe_free((void **)&state);
+	ft_printf("Bye :D\n");
+	exit(0);
+}
+
+void	key_hook(mlx_key_data_t keydata, void *param)
 {
 	t_state	*state;
 
 	state = (t_state *)param;
 	if (keydata.action == MLX_PRESS && keydata.key == MLX_KEY_ESCAPE)
-	{
-		ft_printf("Bye :D\n");
-		clean_map(state->map);
-		mlx_terminate(state->mlx);
-	}
+		return (close_hook(param));
 }
