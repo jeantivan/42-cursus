3c809e9359638157294b35e39ee5788d492709b9
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2025-01-21 18:57:08 +0100

    ADD: Limpia el estado y termina el programa con un mensaje de error

--- circle-02/FdF/error.c
+++ circle-02/FdF/error.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 15:07:59 by jtivan-r          #+#    #+#             */
-/*   Updated: 2024/12/11 15:08:21 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2025/01/21 18:47:28 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,11 @@ void	ft_error(char *error_message)
 {
 	ft_printf("Error: %s\n", error_message);
 	exit(EXIT_FAILURE);
+}
+
+void	*clean_with_error(t_state *state, char *error_message)
+{
+	clean_map(state->map);
+	ft_error(error_message);
+	return (NULL);
 }
