e76f1ff5aa64c64e054f7fc8788964f1547748c6
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2025-02-03 17:45:16 +0100

    CHANGE: Para manejar bien el escalado con los hooks

--- circle-02/FdF/map_utils.c
+++ circle-02/FdF/map_utils.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 17:16:34 by jtivan-r          #+#    #+#             */
-/*   Updated: 2025/02/03 16:45:43 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2025/02/03 17:32:04 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,7 @@ void	prepare_map(t_map *map, t_point *projection, size_t len, bool fit)
 	parse_map(map, projection);
 	if (fit)
 	{
+		map->scale = 1;
 		while (points_fit(projection, len))
 		{
 			copy_points(projection, map->points, len);
