e3a21bef8827a49a36cbbf4a19f72679a715f1bd
Jean Tivan Rendon <jtivan-r@student.42madrid.com>
2025-02-11 18:19:21 +0100

    CREATE: Libera la memoria creada por la ejecucion de un split

---
+++ circle-02/pipex/libft/ft_free_split.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_free_split.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtivan-r <jtivan-r@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 11:24:19 by jtivan-r          #+#    #+#             */
+/*   Updated: 2025/02/11 11:25:16 by jtivan-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_free_split(char **splited)
+{
+	int	i;
+
+	i = 0;
+	if (!splited)
+		return (NULL);
+	while (splited[i])
+	{
+		ft_safe_free((void **)&splited[i]);
+		i++;
+	}
+	return (ft_safe_free((void **)&splited));
+}
