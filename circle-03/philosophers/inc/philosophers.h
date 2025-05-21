297d6979b472d9a4ca587366a6d51c1fee56b4d7
Jean Tivan <jptivan53@gmail.com>
2025-05-21 19:40:00 +0200

    ADD: Nuevos prototipos para las funciones en fork.c y para safe_free

--- circle-03/philosophers/inc/philosophers.h
+++ circle-03/philosophers/inc/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 11:15:19 by jtivan-r          #+#    #+#             */
-/*   Updated: 2025/05/17 21:09:09 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2025/05/21 19:39:29 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,11 @@ void		ft_putstr_fd(char *s, int fd);
 int			ft_atoi(const char *nptr);
 long int	ft_atol(const char *nptr);
 bool		valid_args(int ac, char **av);
+void		*safe_free(void **ptr);
+
+/* Fork */
+bool		create_forks(t_table *table);
+void		clean_forks(t_fork *forks, long qty);
 
 /* Error */
 int			ft_puterror(char *str, char *info);
