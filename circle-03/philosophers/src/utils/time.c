84ac544821ba95f2d234de10f02419ef0591072f
Jean Tivan <jptivan53@gmail.com>
2025-06-09 16:08:08 +0200

    CREATE: Utilies para el tiempo y sleep

---
+++ circle-03/philosophers/src/utils/time.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtivan-r <jtivan-r@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/23 13:33:22 by jtivan-r          #+#    #+#             */
+/*   Updated: 2025/05/28 18:33:47 by jtivan-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+long	get_time(t_time_scale scale)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	if (scale == MILI)
+		return (tv.tv_sec * 1e3 + tv.tv_usec / 1e3);
+	else if (scale == MICRO)
+		return (tv.tv_sec * 1e6 + tv.tv_usec);
+	return (-1);
+}
+
+void	my_usleep(long usec, t_table *table)
+{
+	long	start;
+
+	start = get_time(MICRO);
+	while (get_time(MICRO) - start < usec)
+	{
+		if (dinner_finished(table))
+			break ;
+		usleep(100);
+	}
+
+}
