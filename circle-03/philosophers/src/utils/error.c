23f489d6eabf1564964d0e03f2ef845af50a0041
Jean Tivan Rendon <jptivan53@gmail.com>
2025-06-16 12:38:53 +0200

    FIX: Nombre de los macros

--- circle-03/philosophers/src/utils/error.c
+++ circle-03/philosophers/src/utils/error.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 19:50:33 by jtivan-r          #+#    #+#             */
-/*   Updated: 2025/05/17 22:06:22 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2025/06/16 12:25:45 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	ft_puterror(char *str, char *info)
 {
-	ft_putstr_fd(BGRED"🚨 Error: "RST, 2);
-	ft_putstr_fd(RED" ", 2);
+	ft_putstr_fd(BGR"🚨 Error: "RST, 2);
+	ft_putstr_fd(R" ", 2);
 	ft_putendl_fd(str, 2);
 	ft_putstr_fd(RST, 2);
 	if (info)
 	{
-		ft_putstr_fd(BGYELLOW"🔎 Info: "RST, 2);
-		ft_putstr_fd(YELLOW" ", 2);
+		ft_putstr_fd(BGY"🔎 Info: "RST, 2);
+		ft_putstr_fd(Y" ", 2);
 		ft_putendl_fd(info, 2);
 		ft_putstr_fd(RST, 2);
 	}
