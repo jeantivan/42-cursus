99686f109d649b1e0e1a5900aec438c5a1b28189
Jean Tivan Rendon <jptivan53@gmail.com>
2025-06-16 13:12:52 +0200

    CHANGE: El proyecto debe ir dentro de la carpeta ./philo

--- circle-03/philosophers/philo/src/write.c
+++ circle-03/philosophers/philo/src/write.c
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 17:40:21 by jtivan-r          #+#    #+#             */
-/*   Updated: 2025/06/16 12:37:25 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2025/06/16 13:07:19 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ void	write_state(t_action action, t_philo *philo)
 	pthread_mutex_lock(&philo->table->print_mtx);
 	if ((action == TAKE_FIRST_FORK || action == TAKE_SECOND_FORK) && \
 	!dinner_finished(philo->table))
-		printf(Y"%-6ld %d has taken a fork 🍴 \n"RST, time_passed, philo->id);
+		printf("%-6ld %d has taken a fork 🍴\n", time_passed, philo->id);
 	else if (action == EAT && !dinner_finished(philo->table))
-		printf(G"%-6ld %d is eating 🍽️\n"RST, time_passed, philo->id);
+		printf("%-6ld %d is eating 🍽️\n", time_passed, philo->id);
 	else if (action == SLEEP && !dinner_finished(philo->table))
-		printf(C"%-6ld %d is sleeping 😴\n"RST, time_passed, philo->id);
+		printf("%-6ld %d is sleeping 😴\n", time_passed, philo->id);
 	else if (action == THINK && !dinner_finished(philo->table))
-		printf(M"%-6ld %d is thinking 🤔\n"RST, time_passed, philo->id);
+		printf("%-6ld %d is thinking 🤔\n", time_passed, philo->id);
 	else if (action == DIE && !dinner_finished(philo->table))
-		printf(R"%-6ld %d has died 😵​\n"RST, time_passed, philo->id);
+		printf("%-6ld %d has died 😵​\n", time_passed, philo->id);
 	pthread_mutex_unlock(&philo->table->print_mtx);
 }
