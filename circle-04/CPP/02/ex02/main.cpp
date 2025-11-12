922c21d9cdc1465ea878a96a0f4b4646d3b2e93d
Jean Tivan Rendon <jptivan53@gmail.com>
2025-11-12 17:43:30 +0100

    ADD: Creados un monton de test para probar la clase

--- circle-04/CPP/02/ex02/main.cpp
+++ circle-04/CPP/02/ex02/main.cpp
@@ -3,19 +3,45 @@
 
 
 int main( void ) {
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed a(5.05f);
+	Fixed const b(2);
+	Fixed c(0);
+	Fixed d(42.42f);
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
+	std::cout << "\n----- Arithmetic operators\n" << std::endl;
+	std::cout << "Add: a " << a << " + b " << b << " = " << a + b << std::endl;
+	std::cout << "Subtract: a " << a << " - d " << d << " = " << a - d << std::endl;
+	std::cout << "Multi: d " << d << " * b " << b << " = " << d * b << std::endl;
+	std::cout << "Div: d " << d << " / a " << a << " = " << d / a << std::endl;
+	std::cout << "Div by zero: a " << a << " / c " << c << " = " << a / c << std::endl;
 
-	std::cout << Fixed(5.05f) / Fixed(0) << std::endl;
+	std::cout << "\n----- Pre & post increment/decrement\n" << std::endl;
 
-	std::cout << Fixed::max( a, b ) << std::endl;
+	Fixed x(12);
+
+	std::cout << "Pre inc: ++x = " << ++x << " x = " << x << std::endl;
+	std::cout << "Post inc: x++ = " << x++ << " x = " << x << std::endl;
+	Fixed y(99.9f);
+	std::cout << "Pre dec: --y = " << --y << " y = " << y << std::endl;
+	std::cout << "Post dec: y-- = " << y-- << " y = " << y << std::endl;
+
+	std::cout << "\n----- Comparison operators\n" << std::endl;
+
+	Fixed n(32);
+	Fixed m(32);
+	Fixed p(42);
+	Fixed q(42);
+
+	std::cout << "'<': n (" << n << ") <  p (" << p << ") : " << (n < p) << std::endl;
+	std::cout << "'>': m (" << m << ") >  p (" << p << ") : " << (m > p) << std::endl;
+	std::cout << "'<=': p (" << p << ") <=  q (" << q << ") : " << (p <= q) << std::endl;
+	std::cout << "'>=': q (" << p << ") >=  n (" << n << ") : " << (p >= n) << std::endl;
+	std::cout << "'==': p (" << p << ") <=  q (" << q << ") : " << (p == q) << std::endl;
+	std::cout << "'!=': p (" << p << ") !=  m (" << m << ") : " << (p != m) << std::endl;
+
+	std::cout << "\n----- Function max & min\n" << std::endl;
+	std::cout << "'max': Fixed::max(" << a << ", " << b << ") = " << Fixed::max( a, b ) << std::endl;
+	std::cout << "'min': Fixed::min(" << y << ", " << d << ") = " << Fixed::min( y, d ) << std::endl;
 
 	return (0);
 }
