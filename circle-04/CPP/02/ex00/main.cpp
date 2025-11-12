76b95c43b31d30adf25bc80643f40a2afb63e32f
Jean Tivan Rendon <jptivan53@gmail.com>
2025-11-12 15:21:39 +0100

    ADD: Saltos de linea para leer mejor lo que sucede

--- circle-04/CPP/02/ex00/main.cpp
+++ circle-04/CPP/02/ex00/main.cpp
@@ -4,14 +4,24 @@
 int main()
 {
 	Fixed a;
+	std::cout << std::endl;
 	Fixed b(a);
+	std::cout << std::endl;
 	Fixed c;
-
+	std::cout << std::endl;
 	c = b;
+	std::cout << std::endl;
+	c.setRawBits(42);
+	std::cout << std::endl;
 
 	std::cout << a.getRawBits() << std::endl;
+	std::cout << std::endl;
+
 	std::cout << b.getRawBits() << std::endl;
+	std::cout << std::endl;
+
 	std::cout << c.getRawBits() << std::endl;
+	std::cout << std::endl;
 
 	return (0);
 }
