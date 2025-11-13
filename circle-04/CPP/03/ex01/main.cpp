ec0b1e38e403a6299727e0401edff54f20d4d3b0
Jean Tivan <jptivan53@gmail.com>
2025-11-13 13:05:57 +0100

    CHANGE: Mejor display de los  test

--- circle-04/CPP/03/ex01/main.cpp
+++ circle-04/CPP/03/ex01/main.cpp
@@ -8,44 +8,61 @@ int main() {
 
 	ScavTrap  * robots[totNames];
 
-	std::cout << "Test constructors" << std::endl;
+	std::cout << "\n---- Test constructors\n" << std::endl;
 	for (size_t  i = 0; i  < totNames; i++)
 	{
 		robots[i] = new ScavTrap(names[i]);
+		std::cout << "\n";
 	}
 	std::cout << std::endl;
 
-	std::cout << "Test copy constructors" << std::endl;
+	std::cout << "\n---- Test copy constructors" << std::endl;
 	ScavTrap gohan(*robots[2]);
-	ScavTrap trunks = *robots[3];
+	std::cout << "\n";
+
+	ScavTrap trunks("trunks");
+	trunks = *robots[3];
 
 	std::cout << std::endl;
 
-	std::cout << "Test inherit functions" << std::endl;
+	std::cout << "\n---- Test inherit functions" << std::endl;
 	robots[3]->attack(robots[0]->getName());
+	std::cout << "\n";
+
 	gohan.attack(robots[0]->getName());
+	std::cout << "\n";
+
 	robots[0]->takeDamage(robots[3]->getAttackDamage());
+	std::cout << "\n";
+
 	robots[0]->takeDamage(gohan.getAttackDamage());
+	std::cout << "\n";
 
 	robots[0]->beRepaired(30);
 	std::cout << "ScavTrap: " << robots[0]->getName() << " health " << robots[0]->getHitPoints() << std::endl;
 
 	std::cout << std::endl;
 
-	std::cout << "Test new functions" << std::endl;
+	std::cout << "\n---- Test new functions\n" << std::endl;
 	robots[1]->guardGate();
+	std::cout << "\n";
+
 	robots[3]->guardGate();
+	std::cout << "\n";
 
 	robots[1]->leaveGate();
-	robots[2]->leaveGate();
+	std::cout << "\n";
 
+	robots[2]->leaveGate();
 	std::cout << std::endl;
 
 
-	std::cout << "Test destructors" << std::endl;
+	std::cout << "\n---- Test destructors\n" << std::endl;
 	for (size_t  i = 0; i  < totNames; i++)
 	{
 		delete robots[i];
+		std::cout << "\n";
 	}
+
 	return (0);
 }
