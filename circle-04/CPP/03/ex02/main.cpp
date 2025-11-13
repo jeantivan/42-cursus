2ca0967c926766af346f37047b30f66e8b8eed6b
Jean Tivan Rendon <jptivan53@gmail.com>
2025-11-13 18:35:06 +0100

    ADD: Algunos test mas

--- circle-04/CPP/03/ex02/main.cpp
+++ circle-04/CPP/03/ex02/main.cpp
@@ -8,16 +8,20 @@ int main() {
     std::cout << "\n=== Creación con nombre ===" << std::endl;
     FragTrap ft2("Bob");
 
-    std::cout << "\n=== Prueba de método especial ===" << std::endl;
-    ft2.highFivesGuys();
-
     std::cout << "\n=== Copia de objeto (constructor de copia) ===" << std::endl;
     FragTrap ft3(ft2);
 
     std::cout << "\n=== Asignación de objetos (operator=) ===" << std::endl;
     ft1 = ft3;
 
+    std::cout << "\n=== Prueba de método especial ===" << std::endl;
+    ft2.highFivesGuys();
+
+    std::cout << "\n=== Prueba de método heredado ===" << std::endl;
+    FragTrap ft4("Alex");
+    ft4.attack(ft3.getName());
+    ft4.showStats();
+
     std::cout << "\n=== Fin del main, comienza destrucción ===" << std::endl;
-    return 0;
 	return 0;
 }
