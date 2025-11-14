3b4c0d75e0c977765e1d09bf576e73277ba3299e
Jean Tivan Rendon <jptivan53@gmail.com>
2025-11-14 20:08:13 +0100

    ADD: Codigo mas claro para los test

--- circle-04/CPP/03/ex01/main.cpp
+++ circle-04/CPP/03/ex01/main.cpp
@@ -5,36 +5,52 @@
 int main() {
 	std::cout << "\n---- Named constructor\n";
 	// Named Constructor
-	ScavTrap scav1("Goku");
+	ScavTrap goku("Goku");
 	std::cout << "\n";
 
 	// Copy constructor
-	ClapTrap * scav2 = new ScavTrap("Vegeta");
+	ClapTrap * vegeta = new ScavTrap("Vegeta");
 	std::cout << "\n";
 
 	// Default constructor
-	ScavTrap scav3;
+	ScavTrap trunks;
 	std::cout << "\n";
 
 	// Magic to use the operator '='
-	scav3 = *static_cast<ScavTrap *>(scav2);
+	trunks = *static_cast<ScavTrap *>(vegeta);
+	std::cout << "\n";
+	trunks = ScavTrap("Trunks");
 	std::cout << "\n";
 
 	// Inherited function
-	scav1.showStats();
+	goku.showStats();
 	std::cout << "\n";
 
-	scav2->showStats();
+	vegeta->showStats();
 	std::cout << "\n";
 
-	scav3.showStats();
+	trunks.showStats();
 	std::cout << "\n";
 
-	scav1.attack(scav2->getName());
+	goku.attack(vegeta->getName());
 	std::cout << "\n";
 
-	scav2->takeDamage(scav1.getAttackDamage());
+	vegeta->takeDamage(goku.getAttackDamage());
 	std::cout << "\n";
 
+	vegeta->attack(trunks.getName());
+	std::cout << "\n";
+
+	trunks.takeDamage(vegeta->getAttackDamage());
+	std::cout << "\n";
+
+	// Own functions
+	goku.guardGate();
+	std::cout << "\n";
+
+	(static_cast<ScavTrap *>(vegeta))->guardGate();
+	std::cout << "\n";
+	// Free de ScavTrap pointer
+	delete vegeta;
 	return (0);
 }
