4954e1d33ee3837d4410aa8aa3b2decb1ec5bc59
Jean Tivan Rendon <jptivan53@gmail.com>
2025-11-18 18:54:37 +0100

    ADD: Test mas cuidados

--- circle-04/CPP/03/ex00/main.cpp
+++ circle-04/CPP/03/ex00/main.cpp
@@ -5,10 +5,15 @@ int main()
 {
 {
 	std::cout << "\n----  Orthodox canonical form\n" << std::endl;
+	std::cout << "Default constructor\n";
 	ClapTrap c1;
-	ClapTrap c2("Megatron");
-	ClapTrap c3("Optimus");
-	c1 = c3;
+	std::cout << "\nParameter constructor\n";
+	ClapTrap c2("Optimus");
+	std::cout << "\nCopy constructor\n";
+	ClapTrap c3(c1);
+	std::cout << "\nAssign operator\n";
+	c1 = c2;
+	std::cout << "\nDestructor\n";
 }
 {
 	std::cout << "\n----  Testing functions\n" << std::endl;
@@ -19,19 +24,18 @@ int main()
 	clap.takeDamage(2);
 	clap.takeDamage(1);
 	clap.takeDamage(2);
+	clap.showStats();
 
 	std::cout << "\n--  Repairing damage\n" << std::endl;
 	clap.beRepaired(2);
 	clap.beRepaired(12);
 	clap.beRepaired(1);
 
-	clap.takeDamage(2);
+	clap.takeDamage(42);
+	clap.showStats();
+	clap.beRepaired(42);
+	clap.showStats();
 
-	std::cout << "\n--  Testing copy\n" << std::endl;
-	ClapTrap c3;
-	c3 = clap;
-
-	c3.takeDamage(1);
 }
 	return 0;
 }
