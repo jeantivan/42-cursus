bb4e89e3565019439b475c24a04bb9e20a59268b
Jean Tivan Rendon <jptivan53@gmail.com>
2025-11-18 19:19:13 +0100

    CHANGE: Version final de ClapTrap para este ejercicio

--- circle-04/CPP/03/ex01/ClapTrap.cpp
+++ circle-04/CPP/03/ex01/ClapTrap.cpp
@@ -4,37 +4,30 @@ const unsigned int ClapTrap::MAX_HIT_POINTS = 10;
 const unsigned int ClapTrap::MAX_ENERGY_POINTS = 10;
 const unsigned int ClapTrap::MAX_ATTACK_DAMAGE = 0;
 
-ClapTrap::ClapTrap() : _name("Bumblebee"), _hit_points(MAX_HIT_POINTS), _energy_points(MAX_ENERGY_POINTS), _attack_damage(MAX_ATTACK_DAMAGE) {
+ClapTrap::ClapTrap() : _name("Default"), _hit_points(MAX_HIT_POINTS), _energy_points(MAX_ENERGY_POINTS), _attack_damage(MAX_ATTACK_DAMAGE) {
 	std::cout << "I'm " << _name << " ready to fight! " << std::endl;
 }
 
-ClapTrap::ClapTrap(const std::string name) : _name(name), _hit_points(10), _energy_points(10), _attack_damage(0) {
+ClapTrap::ClapTrap(const std::string name) : _name(name), _hit_points(MAX_HIT_POINTS), _energy_points(MAX_ENERGY_POINTS), _attack_damage(MAX_ATTACK_DAMAGE) {
 	std::cout << "I'm " << _name << " ready to fight!" << std::endl;
 }
 
-ClapTrap::ClapTrap(const ClapTrap &other) {
-	_name = other._name;
-	_hit_points = other._hit_points;
-	_energy_points = other._energy_points;
-	_attack_damage = other._attack_damage;
+ClapTrap::ClapTrap(const ClapTrap &other) : _name(other._name), _hit_points(other._hit_points), _energy_points(other._energy_points), _attack_damage(other._attack_damage) {
 	std::cout << "A new " << _name << " has emerged!" << std::endl;
 }
 
 ClapTrap::~ClapTrap() {
-	std::cout << "ClapTrap: " << _name << " was destroyed!" << std::endl;
+	std::cout << _name << " was destroyed!" << std::endl;
 }
 
-
 ClapTrap& ClapTrap::operator=(const ClapTrap &other) {
-	if (this != &other)
-	{
+	if (this != &other) {
 		_name = other._name;
 		_hit_points = other._hit_points;
 		_energy_points = other._energy_points;
 		_attack_damage = other._attack_damage;
 	}
 	std::cout << _name << " was successfully cloned!" << std::endl;
-
 	return *this;
 }
 
@@ -63,20 +56,20 @@ void ClapTrap::attack(const std::string &target) {
 }
 
 void ClapTrap::takeDamage(unsigned int amount) {
-	int new_hit_points = static_cast<int>(_hit_points) - static_cast<int>(amount);
-
 	if (_hit_points == 0)
 	{
 		std::cout << "ClapTrap " << _name << " can't take more damage. Health is " << _hit_points << std::endl;
 		return ;
 	}
-	std::cout << "ClapTrap " << _name << " took " << amount << " points of damage!" << std::endl;
+	int new_hit_points = static_cast<int>(_hit_points) - static_cast<int>(amount);
 	if (new_hit_points > 0)
-	{
 		_hit_points = new_hit_points;
-	}
 	else
+	{
+		amount = _hit_points;
 		_hit_points = 0;
+	}
+	std::cout << "ClapTrap " << _name << " took " << amount << " points of damage!" << std::endl;
 	std::cout << _name << " health " << _hit_points << std::endl;
 
 }
@@ -95,11 +88,19 @@ void ClapTrap::beRepaired(unsigned int amount) {
 	}
 
 	if (_hit_points + amount >= MAX_HIT_POINTS)
-		amount = MAX_HIT_POINTS - amount;
+		amount = MAX_HIT_POINTS - _hit_points;
 	_hit_points += amount;
 	_energy_points--;
 	std::cout << "ClapTrap " << _name << " regained " << amount << " health points!" << std::endl;
 }
+
+void ClapTrap::showStats() {
+	std::cout << _name << " stats:\n";
+	std::cout << " -- hit points: " << _hit_points << "\n";
+	std::cout << " -- energy points: " << _energy_points << "\n";
+	std::cout << " -- attack damage: " << _attack_damage << std::endl;
+}
+
 
 // Getters & Setters
 std::string ClapTrap::getName() const {
@@ -136,11 +137,4 @@ void ClapTrap::setEnergyPoints(unsigned int n)
 void ClapTrap::setAttackDamage(unsigned int n)
 {
 	_attack_damage = n;
-}
-
-void ClapTrap::showStats() {
-	std::cout << _name << " stats:\n";
-	std::cout << " -- hit points: " << _hit_points << "\n";
-	std::cout << " -- energy points: " << _energy_points << "\n";
-	std::cout << " -- attack damage: " << _attack_damage << std::endl;
 }
