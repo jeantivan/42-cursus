159833a5ae72faf668b33dc44a422f442263c66e
Jean Tivan Rendon <jptivan53@gmail.com>
2025-11-19 16:14:00 +0100

    CHANGE: Test de la clase

--- circle-04/CPP/03/ex02/main.cpp
+++ circle-04/CPP/03/ex02/main.cpp
@@ -2,27 +2,91 @@
 #include "FragTrap.hpp"
 
 int main() {
-	std::cout << "=== Creación por defecto ===" << std::endl;
+{
+    std::cout << "\n====== Test Canonical orthodox form ======\n";
+
+    std::cout << "\n=== Default constructor ===\n\n";
     FragTrap ft1;
 
-    std::cout << "\n=== Creación con nombre ===" << std::endl;
+    std::cout << "\n=== Parameter constructor ===\n\n";
     FragTrap ft2("Bob");
 
-    std::cout << "\n=== Copia de objeto (constructor de copia) ===" << std::endl;
+    std::cout << "\n=== Copy constructor ===\n\n";
+    ft2.takeDamage(42);
     FragTrap ft3(ft2);
+    ft2.showStats();
+    ft3.showStats();
 
-    std::cout << "\n=== Asignación de objetos (operator=) ===" << std::endl;
+    std::cout << "\n=== Assign operator ===\n";
     ft1 = ft3;
+    ft2.showStats();
+    ft3.showStats();
 
-    std::cout << "\n=== Prueba de método especial ===" << std::endl;
-    ft2.highFivesGuys();
+    std::cout << "\n=== Destructors ===\n\n";
 
-    std::cout << "\n=== Prueba de método heredado ===" << std::endl;
-    FragTrap ft4("Alex");
-    ft4.attack(ft3.getName());
-    ft4.takeDamage(10001);
-    ft4.beRepaired(10000);
+}
 
-    std::cout << "\n=== Fin del main, comienza destrucción ===" << std::endl;
+{
+    std::cout << "\n====== Test Inheritance ======\n\n";
+
+    ClapTrap *frag = new FragTrap("Frag-PTR");
+
+    std::cout << "\n=== Own member function ===\n\n";
+    static_cast<FragTrap *>(frag)->highFivesGuys();
+
+    std::cout << "\n=== Inherited but overwritten functions  ===\n\n";
+
+    frag->showStats();
+    std::cout << "\n";
+
+    frag->beRepaired(100);
+    std::cout << "\n";
+
+    frag->takeDamage(42);
+    std::cout << "\n";
+
+    frag->showStats();
+    std::cout << "\n";
+
+    frag->beRepaired(12);
+    std::cout << "\n";
+
+
+    frag->showStats();
+    std::cout << "\n";
+
+    frag->beRepaired(42);
+    std::cout << "\n";
+
+    frag->showStats();
+    std::cout << "\n";
+
+    frag->attack("you");
+    std::cout << "\n";
+
+    // for (int i = 0; i < 100; i++)
+    //     frag->attack("you");
+
+    // std::cout << "\n";
+
+    // frag->beRepaired(100);
+
+    // frag->takeDamage(42);
+
+    // frag->showStats();
+
+    std::cout << "\n=== Delete pointer ===\n\n";
+    delete frag;
+}
+
+
+
+
+
+
+
+
+
+
 	return 0;
 }
