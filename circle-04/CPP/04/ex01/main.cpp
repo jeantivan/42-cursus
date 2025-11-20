d08aa08c89d4226abdad4a8bef444d84d02195e2
Jean Tivan Rendon <jptivan53@gmail.com>
2025-11-20 20:03:05 +0100

    WIP: Mas test

--- circle-04/CPP/04/ex01/main.cpp
+++ circle-04/CPP/04/ex01/main.cpp
@@ -4,19 +4,49 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
-#include "WrongAnimal.hpp"
-#include "WrongCat.hpp"
-
 int main() {
+	std::cout << "\n===== Canonical orthodox form =====\n";
 
-Dog basic;
+	std::cout << "\n--- Creating a Dog ---\n\n";
+	Dog dog1;
+	std::cout << "\n--- Creating a Cat ---\n\n";
+	Cat cat1;
 
-std::cout << "\n---------\n";
+	std::cout << "\n--- Copy the Dog ---\n\n";
+	Dog dog2(dog1);
+	std::cout << "\n--- Copy the Cat ---\n\n";
+	Cat cat2(cat1);
 
-{
-	Dog tmp = basic;
-}
+	std::cout << "\n--- Assign the Dog ---\n\n";
+	Dog dog3;
+	dog3 = dog1;
+	std::cout << "\n--- Assign the Cat ---\n\n";
+	Cat cat3;
+	cat3 = cat1;
 
-std::cout << "\n---------\n";
-	return 0;
+	std::cout << "\n--- Check deep copy of Dog ---\n\n";
+
+	std::cout << "dog1 brain address " << dog1.getBrain() << " \n";
+	std::cout << "dog2 brain address " << dog2.getBrain() << " \n";
+	std::cout << "dog3 brain address " << dog3.getBrain() << " \n";
+
+	std::cout << "\n--- dogN idea '# 42' address ---\n\n";
+
+	std::cout << "dog1.brain->ideas[42] " << &dog1.getBrain()->getIdea(42) << " \n";
+	std::cout << "dog2.brain->ideas[42]  " << &dog2.getBrain()->getIdea(42)  << " \n";
+	std::cout << "dog3.brain->ideas[42]  " << &dog3.getBrain()->getIdea(42)  << " \n";
+
+	std::cout << "\n--- Check deep copy of Cat ---\n\n";
+
+	std::cout << "cat1 brain address " << cat1.getBrain() << " \n";
+	std::cout << "cat2 brain address " << cat2.getBrain() << " \n";
+	std::cout << "dog3 brain address " << dog3.getBrain() << " \n";
+
+	std::cout << "\n--- catN idea '# 42' address ---\n\n";
+
+	std::cout << "cat1.brain->ideas[42] " << &cat1.getBrain()->getIdea(42) << " \n";
+	std::cout << "cat2.brain->ideas[42]  " << &cat2.getBrain()->getIdea(42)  << " \n";
+	std::cout << "cat3.brain->ideas[42]  " << &cat3.getBrain()->getIdea(42)  << " \n";
+
+	std::cout << "\n-- Automatic destruction\n\n";
 }
