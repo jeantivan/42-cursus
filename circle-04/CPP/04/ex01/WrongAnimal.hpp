032fa863228f1f7738154cff876fd781bc4bcaa6
Jean Tivan Rendon <jptivan53@gmail.com>
2025-11-21 18:32:04 +0100

    CHANGE: WrongAnimal no debe tener nada virtual

--- circle-04/CPP/04/ex01/WrongAnimal.hpp
+++ circle-04/CPP/04/ex01/WrongAnimal.hpp
@@ -12,11 +12,11 @@ public:
 	WrongAnimal();
 	WrongAnimal(const WrongAnimal &other);
 	WrongAnimal(const std::string type);
-	virtual ~WrongAnimal();
+	~WrongAnimal();
 	WrongAnimal &operator=(const WrongAnimal &other);
 
 	std::string getType() const;
-	virtual void makeSound() const;
+	void makeSound() const;
 };
 
 #endif // WRONGANIMAL_HPP
