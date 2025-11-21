ad02c3c21dc8679c47772f28bd3e741040e20c0c
Jean Tivan Rendon <jptivan53@gmail.com>
2025-11-21 19:00:15 +0100

    CHANGE: Nuevos test, basicamente son los del ex01 mas unos pocos para probar la clase abstracta

--- circle-04/CPP/04/ex02/main.cpp
+++ circle-04/CPP/04/ex02/main.cpp
@@ -4,40 +4,81 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
-#include "WrongAnimal.hpp"
-#include "WrongCat.hpp"
-
 int main() {
-	std::cout << std::setw(42) << std::setfill('-') << " Dog constructor \n" << std::endl;
-	Animal* dog = new Dog();
-	Animal* dog2 = new Dog(*(Dog *)dog);
-	std::cout << std::endl;
+{
+	std::cout << "\n===== Canonical orthodox form =====\n";
 
-	std::cout << std::setw(42) << std::setfill('-') << " Dog destructor \n" << std::endl;
-	delete dog;
-	delete dog2;
-	std::cout << std::endl;
+	std::cout << "\n--- Creating a Dog ---\n\n";
+	Dog dog1;
+	std::cout << "\n--- Creating a Cat ---\n\n";
+	Cat cat1;
 
-	std::cout << std::setw(42) << std::setfill('-') << " Cat constructor \n" << std::endl;
-	Animal* cat = new Cat();
-	Animal* cat2 = new Cat(*(Cat *)cat);
-	std::cout << std::endl;
+	std::cout << "\n--- Copy the Dog ---\n\n";
+	Dog dog2(dog1);
+	std::cout << "\n--- Copy the Cat ---\n\n";
+	Cat cat2(cat1);
 
+	std::cout << "\n--- Assign the Dog ---\n\n";
+	Dog dog3;
+	dog3 = dog1;
+	std::cout << "\n--- Assign the Cat ---\n\n";
+	Cat cat3;
+	cat3 = cat1;
 
-	std::cout << std::setw(42) << std::setfill('-') << " Cat destructor \n" << std::endl;
+	std::cout << "\n--- Check deep copy of Dog ---\n\n";
+
+	std::cout << "dog1 brain address " << dog1.getBrain() << " \n";
+	std::cout << "dog2 brain address " << dog2.getBrain() << " \n";
+	std::cout << "dog3 brain address " << dog3.getBrain() << " \n";
+
+	std::cout << "\n--- dogN idea '# 42' address ---\n\n";
+
+	std::cout << "dog1.brain->ideas[42] " << &dog1.getBrain()->getIdea(42) << " \n";
+	std::cout << "dog2.brain->ideas[42]  " << &dog2.getBrain()->getIdea(42)  << " \n";
+	std::cout << "dog3.brain->ideas[42]  " << &dog3.getBrain()->getIdea(42)  << " \n";
+
+	std::cout << "\n--- Check deep copy of Cat ---\n\n";
+
+	std::cout << "cat1 brain address " << cat1.getBrain() << " \n";
+	std::cout << "cat2 brain address " << cat2.getBrain() << " \n";
+	std::cout << "cat3 brain address " << cat3.getBrain() << " \n";
+
+	std::cout << "\n--- catN idea '# 42' address ---\n\n";
+
+	std::cout << "cat1.brain->ideas[42] " << &cat1.getBrain()->getIdea(42) << " \n";
+	std::cout << "cat2.brain->ideas[42]  " << &cat2.getBrain()->getIdea(42)  << " \n";
+	std::cout << "cat3.brain->ideas[42]  " << &cat3.getBrain()->getIdea(42)  << " \n";
+
+	std::cout << "\n-- Automatic destruction\n\n";
+}
+
+{
+	std::cout << "\n===== Some test more =====\n\n";
+
+	Animal *dog = new Dog();
+
+	dog->makeSound();
+
+	std::cout << "\n";
+
+	Animal *cat = new Cat();
+
+	cat->makeSound();
+
+	std::cout << "\n";
+
+	std::cout << "Dog type " << dog->getType() << "\n";
+	std::cout << "Cat type " << cat->getType() << "\n";
+
+	std::cout << "\n";
+
+	// Compiler error
+	// Animal *animal = new Animal();
+	// Animal animal;
+
 	delete cat;
-	delete cat2;
-	std::cout << std::endl;
+	std::cout << "\n";
+	delete dog;
+}
 
-	Cat gatito;
-	std::cout << "Gatito type: " << gatito.getType() << std::endl;
-	gatito.makePooPoo();
-
-	Dog scooby;
-	std::cout << "Scooby type: " << scooby.getType() << std::endl;
-	scooby.makePooPoo();
-
-	// Fail to compile because Animal is an abstract class
-	//Animal animal;
-	return 0;
 }
