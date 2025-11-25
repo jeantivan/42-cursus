03ba33263a5a98a7612faf44f34e7a01c1925823
Jean Tivan Rendon <jptivan53@gmail.com>
2025-11-25 17:29:41 +0100

    CREATE: Donde estaran los test

---
+++ circle-05/CPP/05/ex01/test.cpp
@@ -0,0 +1,47 @@
+#include "test.hpp"
+
+void correct_form() {
+	std::cout << "\n====    Correct forms    ====\n\n";
+	try
+	{
+		Form def_form;
+		std::cout << def_form << "\n";
+
+		Form custom_form("Custom form", 42, 4);
+		std::cout << custom_form << "\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << '\n';
+	}
+}
+
+void grade_too_high_form() {
+	std::cout << "\n====    Grade too high    ====\n\n";
+	try
+	{
+		Form form("High grade", 1, 1);
+		std::cout << form << "\n";
+		Form form_high("Grade too high", -42, 0);
+		std::cout << form_high << "\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << '\n';
+	}
+}
+
+void grade_too_low_form() {
+	std::cout << "\n====    Grade too low    ====\n\n";
+	try
+	{
+		Form form_low("Grade too low", 150, 42);
+		std::cout << form_low << "\n";
+		Form form_too_low("Grade too low", 4242, 42);
+		std::cout << form_too_low << "\n";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << '\n';
+	}
+}
---
+++ circle-05/CPP/05/ex01/test.hpp
@@ -0,0 +1,11 @@
+#ifndef TEST_HPP
+# define TEST_HPP
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+void correct_form();
+void grade_too_high_form();
+void grade_too_low_form();
+
+#endif // TEST_HPP
