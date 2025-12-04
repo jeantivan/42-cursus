1157b5d916c3d6067be1c2b803ecbe1be692d66f
Jean Tivan <jptivan53@gmail.com>
2025-12-04 22:43:59 +0100

    ADD: Test para el ejercicio

--- circle-05/CPP/05/ex03/main.cpp
+++ circle-05/CPP/05/ex03/main.cpp
@@ -2,14 +2,30 @@
 #include <cstdlib>
 #include <ctime>
 
-#include "test.hpp"
+#include "Intern.hpp"
 
 int main()
 {
 	srand((unsigned)time(NULL));
 
-	no_signed_forms();
-	buro_grade_too_low_to_execute();
-	correct_execution();
+	Intern intern;
+
+	try {
+		AForm* shrubbery = intern.makeForm("shrubbery creation", "manzanas");
+		std::cout << *shrubbery << "\n";
+
+		AForm* robotomy = intern.makeForm("robotomy request", "God");
+		std::cout << *robotomy << "\n";
+
+		AForm* presidential = intern.makeForm("presidential pardon", "John");
+		std::cout << *presidential << "\n";
+
+		AForm* impossible = intern.makeForm("kickflip varial", "Tony Hawks");
+		std::cout << *impossible << "\n";
+
+	} catch (std::exception& e)
+	{
+		std::cout << RED << "Exception: " << e.what() << RST"\n";
+	}
 	return 0;
 }
