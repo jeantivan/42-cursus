67fa58b52aacea446561bb322216d2f0d290df61
Jean Tivan <jptivan53@gmail.com>
2025-12-04 19:19:00 +0100

    ADD: Colorines

--- circle-05/CPP/05/ex02/AForm.hpp
+++ circle-05/CPP/05/ex02/AForm.hpp
@@ -4,6 +4,21 @@
 #include <iostream>
 #include <exception>
 
+#define BOLD_PURPLE  "\033[1;35m"
+#define BOLD_CYAN  "\033[1;36m"
+#define BOLD_YELLOW  "\033[1;33m"
+#define NO_COLOR  "\033[0m"
+#define RST  "\033[0;39m"
+#define GRAY  "\033[0;90m"
+#define RED  "\033[0;91m"
+#define GREEN  "\033[0;92m"
+#define YELLOW  "\033[0;93m"
+#define BLUE  "\033[0;94m"
+#define MAGENTA  "\033[0;95m"
+#define CYAN  "\033[0;96m"
+#define WHITE  "\033[0;97m"
+#define BG_GREEN  "\033[42;37m"
+
 class Bureaucrat;
 
 class AForm
