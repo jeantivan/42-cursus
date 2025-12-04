2d64bca2955832e2c074054a0a4ca7d8e7df43e6
Jean Tivan <jptivan53@gmail.com>
2025-12-04 19:20:57 +0100

    ADD: Form action +  colorines

--- circle-05/CPP/05/ex02/PresidentialPardonForm.cpp
+++ circle-05/CPP/05/ex02/PresidentialPardonForm.cpp
@@ -1,22 +1,22 @@
 #include "PresidentialPardonForm.hpp"
 
 // Canonical orthodox form
-PresidentialPardonForm::PresidentialPardonForm() : AForm("PresidentialPardon Default", 72, 45), target("") {
-	std::cout << "PresidentialPardon 'Default' created\n";
+PresidentialPardonForm::PresidentialPardonForm() : AForm("PresidentialPardon Default", 25, 5), target("") {
+	std::cout << GRAY << "PresidentialPardon 'Default' created" << RST"\n";
 }
 
-PresidentialPardonForm::PresidentialPardonForm(const std::string& t) : AForm("PresidentialPardon " + t, 72, 45), target(t) {
-	std::cout << "PresidentialPardon '" + target + "' created\n";
+PresidentialPardonForm::PresidentialPardonForm(const std::string& t) : AForm("PresidentialPardon " + t, 25, 5), target(t) {
+	std::cout << GRAY << "PresidentialPardon '" + target + "' created" << RST"\n";
 }
 
 PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& other) : AForm(other), target(other.target) {
-	std::cout << "PresidentialPardon copied\n";
+	std::cout << GRAY << "PresidentialPardon copied" << RST"\n";
 }
 
 PresidentialPardonForm::~PresidentialPardonForm() {
-	std::cout << "PresidentialPardon " << getName() << " destroyed\n";
+	std::cout << GRAY << "PresidentialPardon " << getName() << " destroyed" << RST"\n";
 }
 
 void PresidentialPardonForm::formAction() const {
-
+	std::cout << BLUE << target  << " has been pardoned by Zaphod Beeblebrox." << RST"\n";
 }
