8354f3254e8a59af3bec2f848022063b6c6ec03a
Jean Tivan <jptivan53@gmail.com>
2025-12-16 00:28:51 +0100

    CREATE: Esqueleto base de la clase

---
+++ circle-05/CPP/06/ex01/Serializer.hpp
@@ -0,0 +1,20 @@
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+#include "Data.hpp"
+
+class Serializer
+{
+private:
+	Serializer();
+	Serializer(const Serializer &other);
+	~Serializer();
+	Serializer &operator=(const Serializer &other);
+
+public:
+	static uintptr_t serialize(Data* ptr);
+	static Data* deserialize(uintptr_t raw);
+
+};
+
+#endif // SERIALIZER_HPP
