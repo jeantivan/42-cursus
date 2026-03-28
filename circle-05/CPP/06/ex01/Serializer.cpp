223e0bc04eaefba569b2a933c2188c0eb06f270f
Jean Tivan <jptivan53@gmail.com>
2026-03-28 20:58:30 +0100

    ADD: include de Data.hpp

--- circle-05/CPP/06/ex01/Serializer.cpp
+++ circle-05/CPP/06/ex01/Serializer.cpp
@@ -1,4 +1,5 @@
 #include "Serializer.hpp"
+#include "Data.hpp"
 
 // This methods don't work because the class can't be instantiated
 Serializer::Serializer() {}
@@ -7,13 +8,19 @@ Serializer::~Serializer() {}
 
 Serializer::Serializer(const Serializer &other) { (void)other; }
 
-Serializer& Serializer::operator=(const Serializer &other)  { (void)other; return *this; }
+Serializer &Serializer::operator=(const Serializer &other)
+{
+	(void)other;
+	return *this;
+}
 
 // Static methods
-uintptr_t Serializer::serialize(Data *data) {
+uintptr_t Serializer::serialize(Data *data)
+{
 	return reinterpret_cast<uintptr_t>(data);
 }
 
-Data* Serializer::deserialize(uintptr_t raw) {
+Data *Serializer::deserialize(uintptr_t raw)
+{
 	return reinterpret_cast<Data *>(raw);
 }
