5ed90a6f5ed623e8fe6f970cd7b15f9cfe80ea91
Jean Tivan <jptivan53@gmail.com>
2026-03-29 14:05:24 +0200

    FIX: Error imprimiendo la misma variable siempre

--- circle-05/CPP/07/ex02/main.cpp
+++ circle-05/CPP/07/ex02/main.cpp
@@ -9,36 +9,42 @@ void basic_array()
 	std::cout << "Array size: " << arr.size() << std::endl;
 
 	std::cout << "\nArray elements " << std::endl;
-	for (unsigned int i = 0; i < arr.size(); i++) {
+	for (unsigned int i = 0; i < arr.size(); i++)
+	{
 		std::cout << arr[i] << std::endl;
 	}
 
 	std::cout << "\nArray new elements " << std::endl;
-	for (unsigned int i = 0; i < arr.size(); i++) {
+	for (unsigned int i = 0; i < arr.size(); i++)
+	{
 		arr[i] = "42";
 		std::cout << arr[i] << std::endl;
 	}
 
 	std::cout << "\nTry access index out of bounds" << std::endl;
-	try {
+	try
+	{
 		std::cout << arr[11] << std::endl;
-	} catch (std::exception& e)
+	}
+	catch (std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 	}
-
 }
 
-void copy_and_assign_array() {
+void copy_and_assign_array()
+{
 	Array<float> arr_float(6);
 	Array<float> arr_float3(12);
 
-	for (unsigned int i = 0; i < arr_float.size(); i++) {
+	for (unsigned int i = 0; i < arr_float.size(); i++)
+	{
 		arr_float[i] = 42.0f + i;
 	}
 
 	Array<float> arr_float2 = arr_float;
-	for (unsigned int i = 0; i < arr_float2.size(); i++) {
+	for (unsigned int i = 0; i < arr_float2.size(); i++)
+	{
 		arr_float2[i] = 21.0f - i;
 	}
 
@@ -46,36 +52,41 @@ void copy_and_assign_array() {
 
 	std::cout << "Array sizes" << std::endl;
 	std::cout << "arr_float size " << arr_float.size() << std::endl;
-	std::cout << "arr_float2 size " << arr_float.size() << std::endl;
-	std::cout << "arr_float3 size " << arr_float.size() << std::endl;
+	std::cout << "arr_float2 size " << arr_float2.size() << std::endl;
+	std::cout << "arr_float3 size " << arr_float3.size() << std::endl;
 
 	std::cout << std::endl;
 
 	std::cout << "arr_float1 " << std::endl;
-	for (unsigned int i = 0; i < arr_float.size(); i++) {
+	for (unsigned int i = 0; i < arr_float.size(); i++)
+	{
 		std::cout << arr_float[i] << std::endl;
 	}
 
 	std::cout << std::endl;
 
 	std::cout << "arr_float2 " << std::endl;
-	for (unsigned int i = 0; i < arr_float2.size(); i++) {
+	for (unsigned int i = 0; i < arr_float2.size(); i++)
+	{
 		std::cout << arr_float2[i] << std::endl;
 	}
 
 	std::cout << std::endl;
 
 	std::cout << "arr_float3 " << std::endl;
-	for (unsigned int i = 0; i < arr_float3.size(); i++) {
+	for (unsigned int i = 0; i < arr_float3.size(); i++)
+	{
 		std::cout << arr_float3[i] << std::endl;
 	}
 }
 
-void const_array() {
+void const_array()
+{
 	Array<int> arr(4);
 
 	std::cout << "Original array" << std::endl;
-	for (unsigned int i = 0; i < arr.size(); i++) {
+	for (unsigned int i = 0; i < arr.size(); i++)
+	{
 		arr[i] = i * i;
 		std::cout << arr[i] << std::endl;
 	}
@@ -83,7 +94,8 @@ void const_array() {
 	std::cout << "\n Const array" << std::endl;
 
 	const Array<int> arr_copy = arr;
-	for (unsigned int i = 0; i < arr_copy.size(); i++) {
+	for (unsigned int i = 0; i < arr_copy.size(); i++)
+	{
 		std::cout << arr_copy[i] << std::endl;
 	}
 
@@ -91,11 +103,15 @@ void const_array() {
 	// arr_copy[2] = 3;
 }
 
-int main() {
-	std::cout << "Test basic array\n" << std::endl;
+int main()
+{
+	std::cout << "Test basic array\n"
+			  << std::endl;
 	basic_array();
-	std::cout << "\nTest copy and assign operations\n" << std::endl;
+	std::cout << "\nTest copy and assign operations\n"
+			  << std::endl;
 	copy_and_assign_array();
-	std::cout << "\nTest const array\n" << std::endl;
+	std::cout << "\nTest const array\n"
+			  << std::endl;
 	const_array();
 }
