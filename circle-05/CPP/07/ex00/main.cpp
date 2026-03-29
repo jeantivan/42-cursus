324ac2f751c302b32c0ea6613169b9c2adfe4754
Jean Tivan <jptivan53@gmail.com>
2026-03-29 13:39:08 +0200

    CHANGE:  Archivo renombrado a 'whatever.hpp'

--- circle-05/CPP/07/ex00/main.cpp
+++ circle-05/CPP/07/ex00/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
-#include "template.hpp"
+#include "whatever.hpp"
 
-int main() {
+int main()
+{
 	int a = 2;
 	int b = 3;
-	::swap( a, b );
+	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 }
