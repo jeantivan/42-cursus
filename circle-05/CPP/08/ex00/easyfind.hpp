21afe91d6e8cd9a585dc86fa2cee220e93e96afa
Jean Tivan <jptivan53@gmail.com>
2026-03-30 15:47:50 +0200

    CHANGE: Pasa argumento por valor y no por referencia

--- circle-05/CPP/08/ex00/easyfind.hpp
+++ circle-05/CPP/08/ex00/easyfind.hpp
@@ -1,20 +1,22 @@
 #ifndef EASYFIND_HPP
-# define EASYFIND_HPP
+#define EASYFIND_HPP
 
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
 
-template<typename T>
-typename T::iterator easyfind(T &container, const int& to_find) {
+template <typename T>
+typename T::iterator easyfind(T &container, const int to_find)
+{
 	typename T::iterator it = std::find(container.begin(), container.end(), to_find);
 	if (it != container.end())
 		return it;
 	throw std::runtime_error("Error: Value not found!");
 }
 
-template<typename T>
-typename T::const_iterator easyfind(const T &container, const int& to_find) {
+template <typename T>
+typename T::const_iterator easyfind(const T &container, const int to_find)
+{
 	typename T::const_iterator it = std::find(container.begin(), container.end(), to_find);
 	if (it != container.end())
 		return it;
