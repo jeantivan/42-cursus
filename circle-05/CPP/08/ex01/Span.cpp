662c0fc3ede45435e91f7c10c2a465c8d38520fe
Jean Tivan <jptivan53@gmail.com>
2026-03-30 16:08:50 +0200

    FIX: El vector 'result' debe tener solo la cantidad de elementos que tiene el Span actualmente

--- circle-05/CPP/08/ex01/Span.cpp
+++ circle-05/CPP/08/ex01/Span.cpp
@@ -6,30 +6,32 @@ Span::Span(unsigned int n) : N_(n), numbers_(std::vector<int>()) {}
 
 Span::~Span() {}
 
-Span::Span(const Span& other) : N_(other.N_), numbers_(other.numbers_) {}
+Span::Span(const Span &other) : N_(other.N_), numbers_(other.numbers_) {}
 
-Span& Span::operator=(const Span& other) {
-	if (this != &other) {
+Span &Span::operator=(const Span &other)
+{
+	if (this != &other)
+	{
 		N_ = other.N_;
 		numbers_ = other.numbers_;
 	}
 	return *this;
 }
 
-void Span::addNumber(int num) {
+void Span::addNumber(int num)
+{
 	if (numbers_.size() == N_)
 		throw std::runtime_error("Max size reached.");
 	numbers_.push_back(num);
 }
 
-
-int Span::shortestSpan() const {
+int Span::shortestSpan() const
+{
 	if (numbers_.empty() || numbers_.size() == 1)
 		throw std::runtime_error("Impossible to get a span, vector too small.");
 
 	std::vector<int> copy = numbers_;
-	std::vector<int> result(N_);
-
+	std::vector<int> result(copy.size());
 
 	std::sort(copy.begin(), copy.end());
 	std::adjacent_difference(copy.begin(), copy.end(), result.begin());
@@ -37,7 +39,8 @@ int Span::shortestSpan() const {
 	return *min_el;
 }
 
-int Span::longestSpan() const {
+int Span::longestSpan() const
+{
 	if (numbers_.empty() || numbers_.size() == 1)
 		throw std::runtime_error("Impossible to get a span, vector too small.");
 
