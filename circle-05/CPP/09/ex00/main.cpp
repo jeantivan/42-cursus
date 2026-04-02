113d31e007995025d801c6d9fd683cefd4525043
Jean Tivan Rendon <jptivan53@gmail.com>
2026-04-02 16:43:16 +0200

    CHANGE: Usamos excepciones

--- circle-05/CPP/09/ex00/main.cpp
+++ circle-05/CPP/09/ex00/main.cpp
@@ -3,8 +3,14 @@
 
 int main()
 {
-	BitcoinExchange exchange;
-	exchange.readFile();
-	exchange.printExchangeRates();
+	try {
+		BitcoinExchange exchange("data.csv");
+		exchange.printExchangeRates();
+	} catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+
 	return 0;
 }
