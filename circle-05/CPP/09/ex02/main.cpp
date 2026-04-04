9d1142268a0bad94dbdebe8524f3c33ba33b2974
Jean Tivan <jptivan53@gmail.com>
2026-04-04 20:12:30 +0200

    ADD: Valida e inserta los numeros pasados por linea de comando en el vector y en el deque

--- circle-05/CPP/09/ex02/main.cpp
+++ circle-05/CPP/09/ex02/main.cpp
@@ -1,8 +1,43 @@
-#include <iostream>
 #include "PmergeMe.hpp"
+#include <iterator>
 
-int main()
+bool isValidNumber(const std::string &str, int &val)
 {
-	std::cout << "Hello, PmergeMe!" << std::endl;
+	int num;
+	char extra;
+	std::istringstream iss(str);
+
+	if (!(iss >> num) || (iss >> extra))
+		return false;
+	if (num < 0)
+		return false;
+
+	val = num;
+	return true;
+}
+
+int main(int ac, char **av)
+{
+	if (ac < 2)
+	{
+		std::cerr << "Usage: " << av[0] << " <numbers...>" << std::endl;
+		return 1;
+	}
+
+	std::vector<int> vec_numbers;
+	std::deque<int> deq_numbers;
+	// Insert numbers into vector and deque from command line arguments
+	for (int i = 1; i < ac; ++i)
+	{
+		int num;
+		if (!isValidNumber(av[i], num))
+		{
+			std::cerr << "Error" << std::endl;
+			return 1;
+		}
+		vec_numbers.push_back(num);
+		deq_numbers.push_back(num);
+	}
+
 	return 0;
 }
