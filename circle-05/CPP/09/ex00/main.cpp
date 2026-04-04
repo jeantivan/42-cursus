d76743cdd23d10abff6b0cef66375281ad2a66ef
Jean Tivan <jptivan53@gmail.com>
2026-04-04 17:05:19 +0200

    ADD: Correcto uso del programa

--- circle-05/CPP/09/ex00/main.cpp
+++ circle-05/CPP/09/ex00/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include "BitcoinExchange.hpp"
 
-int main()
+int main(int ac, char **av)
 {
-	try {
-		BitcoinExchange exchange("data.csv");
-		exchange.printExchangeRates();
-	} catch (const std::exception &e)
+	if (ac != 2)
+	{
+		std::cerr << "Usage: " << av[0] << " <csv_file>" << std::endl;
+		return 1;
+	}
+
+	try
+	{
+		BitcoinExchange exchange;
+		std::string input_file = av[1];
+
+		exchange.calculate(input_file);
+	}
+	catch (const std::exception &e)
 	{
 		std::cerr << e.what() << std::endl;
 		return 1;
