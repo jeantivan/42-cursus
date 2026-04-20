4ce2223be9fe42d0870774086da67d25074e0709
Jean Tivan <jptivan53@gmail.com>
2026-04-20 15:11:37 +0200

    DELETE: Template para medir tiempo de ejecución. CREATE: Templates para crear pares ordenados y otra para hacer la inserción siguiendo los numeros de jacobsthal

--- circle-05/CPP/09/ex02/PmergeMe.hpp
+++ circle-05/CPP/09/ex02/PmergeMe.hpp
@@ -7,25 +7,84 @@
 #include <vector>
 #include <cmath>
 #include <ctime>
+#include <utility>
 
 bool isValidNumber(const std::string &str, int &val);
 size_t jacob(int k);
-
-template <typename Func>
-double measure_time(Func f)
-{
-	std::time_t start, end;
-
-	time(&start);
-
-	f();
-
-	time(&end);
-
-	return double(end - start);
-}
-std::vector<std::pair<int, int> > makeSortedPairs(std::vector<int> &numbers);
-void showVecPairs(std::vector<std::pair<int, int> > &vecPairs);
 std::vector<int> mergeInsertionSort(std::vector<int> &vecToSort);
+std::deque<int> mergeInsertionSort(std::deque<int> &deqToSort);
+
+template <typename InContainer, typename OutContainer>
+void makeSortedPairs(InContainer &numbers, OutContainer &pairs)
+{
+	typedef typename InContainer::value_type T;
+
+	typename InContainer::iterator it = numbers.begin();
+
+	while (it != numbers.end())
+	{
+		T first = *it;
+		++it;
+
+		if (it == numbers.end())
+			break;
+
+		T second = *it;
+		++it;
+
+		if (first >= second)
+			pairs.push_back(std::make_pair(first, second));
+		else
+			pairs.push_back(std::make_pair(second, first));
+	}
+}
+
+template <typename Container>
+void jacobsthalInsert(Container &main_c, Container &pend_c)
+{
+	// We already inserted pend_c[0] so if size <= 1 there is nothing to insert
+	if (pend_c.size() <= 1)
+		return;
+
+	size_t pend_size = pend_c.size();
+	size_t last_inserted_idx = 0; // Inserted before calling this function
+
+	// We start the Jacobsthal sequence at 3
+	size_t prev_j = 1;
+	size_t curr_j = 3;
+
+	// Value type of the elements inside the Container;
+	typedef typename Container::value_type T;
+
+	while (last_inserted_idx < pend_size - 1)
+	{
+		// Current index on base 0
+		size_t curr_jacob_idx = curr_j - 1;
+
+		// if we pass the size of the container we need to adjust to the last valid index
+		if (curr_jacob_idx >= pend_size)
+		{
+			curr_jacob_idx = pend_size - 1;
+		}
+
+		// We insert the current group in reverse order
+		for (size_t j = curr_jacob_idx; j > last_inserted_idx; j--)
+		{
+			T val = pend_c[j];
+
+			typename Container::iterator it = std::lower_bound(main_c.begin(), main_c.end(), val)
+
+												  main_c.insert(it, val);
+		}
+
+		// Update the starting point for the next group
+		last_inserted_idx = curr_jacob_idx;
+
+		// Calculate the next Jacobsthal number
+		size_t next_j = curr_j + 2 * prev_j;
+		prev_j = curr_j;
+		curr_j = next_j;
+	}
+}
 
 #endif // PMERGEME_HPP
